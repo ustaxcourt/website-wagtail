1d43b4c912a97721377ad51e9a4b5e036ab8a264
Cody Seibert <cseibert113@gmail.com>
2024-11-20 13:12:59 -0500

    refactor to use fargate

--- infra/main.tf
+++ infra/main.tf
@@ -1,189 +1,215 @@
+# * Part 1 - Setup.
+locals {
+	container_name = "hello-world-container"
+	container_port = 8000 # ! Must be same port from our Dockerfile that we EXPOSE
+	example = "wagtail-repo"
+}
+
 provider "aws" {
-  region = "us-east-1"
+	region = "us-east-1"
+
+	default_tags {
+		tags = { example = local.example }
+	}
 }
 
-resource "aws_iam_role" "ec2_role" {
-  name               = "ec2-ecr-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
+# * Give Docker permission to pusher Docker Images to AWS.
+data "aws_caller_identity" "this" {}
+data "aws_ecr_authorization_token" "this" {}
+data "aws_region" "this" {}
+locals { ecr_address = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.this.name) }
+provider "docker" {
+	registry_auth {
+		address  = local.ecr_address
+		password = data.aws_ecr_authorization_token.this.password
+		username = data.aws_ecr_authorization_token.this.user_name
+	}
 }
 
-resource "aws_iam_policy_attachment" "ec2_ecr_policy_attachment" {
-  name       = "ec2-ecr-policy-attachment"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  roles      = [aws_iam_role.ec2_role.name]
+# * Part 2 - Build and push Docker image.
+module "ecr" {
+	source  = "terraform-aws-modules/ecr/aws"
+	version = "~> 1.6.0"
+
+	repository_force_delete = true
+	repository_name = local.example
+	repository_lifecycle_policy = jsonencode({
+		rules = [{
+			action = { type = "expire" }
+			description = "Delete all images except a handful of the newest images"
+			rulePriority = 1
+			selection = {
+				countNumber = 3
+				countType = "imageCountMoreThan"
+				tagStatus = "any"
+			}
+		}]
+	})
 }
 
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  description = "Allow SSH access"
+# * Build our Image locally with the appropriate name to push our Image
+# * to our Repository in AWS.
+resource "docker_image" "this" {
+	name = format("%v:%v", module.ecr.repository_url, formatdate("YYYY-MM-DD'T'hh-mm-ss", timestamp()))
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+	build { context = "../website" }
 }
 
-resource "aws_eip" "eip" {
-  instance = aws_instance.wagtail_instance.id
+# * Push our Image to our Repository.
+resource "docker_registry_image" "this" {
+	keep_remotely = true # Do not delete the old image when a new image is built
+	name = resource.docker_image.this.name
 }
 
-resource "aws_instance" "wagtail_instance" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.small"
-  key_name      = "cody-delete-me" 
-  security_groups = [aws_security_group.ec2_sg.name]
+# * Part 3 - Create VPC
+data "aws_availability_zones" "available" { state = "available" }
+module "vpc" {
+	source = "terraform-aws-modules/vpc/aws"
+	version = "~> 3.19.0"
 
-  root_block_device {
-    volume_size = 10
-  }
-
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
-
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y docker
-    systemctl start docker
-    systemctl enable docker
-
-    # Install Docker Compose
-    curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-
-    # Wait for the volume to attach
-    while [ ! -e /dev/xvdf ]; do sleep 1; done
-
-    # Format and mount the attached EBS volume
-    mkfs.ext4 /dev/xvdf
-    mkdir /data
-    mount /dev/xvdf /data
-    echo "/dev/xvdf /data ext4 defaults,nofail 0 2" >> /etc/fstab
-
-    mkdir -p /data/sqlite
-    chown ec2-user:ec2-user /data/sqlite
-
-    usermod -aG docker ec2-user
-    newgrp docker
-
-    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com
-    
-    echo '[Unit]
-    Description=Docker Compose Service
-    After=docker.service
-
-    [Service]
-    Restart=always
-    WorkingDirectory=/home/ec2-user
-    ExecStart=/usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up
-    ExecStop=/usr/local/bin/docker-compose down
-
-    [Install]
-    WantedBy=multi-user.target' | sudo tee /etc/systemd/system/docker-compose.service
-
-    sudo systemctl daemon-reload
-    sudo systemctl enable docker-compose.service
-    sudo systemctl start docker-compose.service
-
-    touch /tmp/user_data_done
-  EOF
-
-  tags = {
-    Name = "wagtail-website-instance"
-  }
+	azs = slice(data.aws_availability_zones.available.names, 0, 2) # Span subnetworks across multiple avalibility zones
+	cidr = "10.0.0.0/16"
+	create_igw = true # Expose public subnetworks to the Internet
+	enable_nat_gateway = true # Hide private subnetworks behind NAT Gateway
+	private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+	public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+	single_nat_gateway = true
 }
 
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "ec2-instance-profile"
-  role = aws_iam_role.ec2_role.name
+# * Part 4 - Create application load balancer
+module "alb" {
+	source  = "terraform-aws-modules/alb/aws"
+	version = "~> 8.4.0"
+
+	load_balancer_type = "application"
+	security_groups = [module.vpc.default_security_group_id]
+	subnets = module.vpc.public_subnets
+	vpc_id = module.vpc.vpc_id
+
+	security_group_rules = {
+		ingress_all_http = {
+			type        = "ingress"
+			from_port   = 80
+			to_port     = 80
+			protocol    = "TCP"
+			description = "HTTP web traffic"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+		egress_all = {
+			type        = "egress"
+			from_port   = 0
+			to_port     = 0
+			protocol    = "-1"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+	}
+
+	http_tcp_listeners = [
+		{
+			# ! Defaults to "forward" action for "target group"
+			# ! at index = 0 in "the target_groups" input below.
+			port               = 80
+			protocol           = "HTTP"
+			target_group_index = 0
+		}
+	]
+
+	target_groups = [
+		{
+			backend_port         = local.container_port
+			backend_protocol     = "HTTP"
+			target_type          = "ip"
+		}
+	]
 }
 
-resource "aws_ebs_volume" "db_volume" {
-  availability_zone = aws_instance.wagtail_instance.availability_zone
-  size              = 20           
-  tags = {
-    Name = "sqlite3-storage"
-  }
+# * Step 5 - Create our ECS Cluster.
+module "ecs" {
+	source  = "terraform-aws-modules/ecs/aws"
+	version = "~> 4.1.3"
+
+	cluster_name = local.example
+
+	# * Allocate 20% capacity to FARGATE and then split
+	# * the remaining 80% capacity 50/50 between FARGATE
+	# * and FARGATE_SPOT.
+	fargate_capacity_providers = {
+		FARGATE = {
+			default_capacity_provider_strategy = {
+				base   = 20
+				weight = 50
+			}
+		}
+		FARGATE_SPOT = {
+			default_capacity_provider_strategy = {
+				weight = 50
+			}
+		}
+	}
 }
 
-resource "aws_volume_attachment" "db_attach" {
-  device_name = "/dev/xvdf"
-  volume_id   = aws_ebs_volume.db_volume.id
-  instance_id = aws_instance.wagtail_instance.id
-  force_detach = true
+# * Step 6 - Create our ECS Task Definition
+data "aws_iam_policy_document" "this" {
+	version = "2012-10-17"
+
+	statement {
+		actions = ["sts:AssumeRole"]
+		effect = "Allow"
+
+		principals {
+			identifiers = ["ecs-tasks.amazonaws.com"]
+			type = "Service"
+		}
+	}
+}
+resource "aws_iam_role" "this" { assume_role_policy = data.aws_iam_policy_document.this.json }
+resource "aws_iam_role_policy_attachment" "this" {
+	policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+	role = resource.aws_iam_role.this.name
+}
+resource "aws_ecs_task_definition" "this" {
+	container_definitions = jsonencode([{
+		environment: [
+			{ name = "MY_INPUT_ENV_VAR", value = "terraform-modified-env-var" }
+		],
+		essential = true,
+		image = resource.docker_registry_image.this.name,
+		name = local.container_name,
+		portMappings = [{ containerPort = local.container_port }],
+	}])
+	cpu = 256
+	execution_role_arn = resource.aws_iam_role.this.arn
+	family = "family-of-${local.example}-tasks"
+	memory = 512
+	network_mode = "awsvpc"
+	requires_compatibilities = ["FARGATE"]
 }
 
-data "aws_caller_identity" "current" {}
+# * Step 7 - Run our application.
+resource "aws_ecs_service" "this" {
+	cluster = module.ecs.cluster_id
+	desired_count = 1
+	launch_type = "FARGATE"
+	name = "${local.example}-service"
+	task_definition = resource.aws_ecs_task_definition.this.arn
 
-resource "null_resource" "setup_compose" {
+	lifecycle {
+		ignore_changes = [desired_count] # Allow external changes to happen without Terraform conflicts, particularly around auto-scaling.
+	}
 
-  triggers = {
-    docker_compose_hash = filemd5("docker-compose.yml")
-  }
+	load_balancer {
+		container_name = local.container_name
+		container_port = local.container_port
+		target_group_arn = module.alb.target_group_arns[0]
+	}
 
-  provisioner "file" {
-    source      = "docker-compose.yml"
-    destination = "docker-compose.yml"
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/key.pem")
-      host        = aws_eip.eip.public_ip
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'waiting for user data done'",
-      "while [ ! -f /tmp/user_data_done ]; do sleep 5; done",
-      "echo 'waiting for Docker daemon to be accessible...'",
-      "while [ ! -S /var/run/docker.sock ]; do sleep 5; done",
-      "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com", 
-      "docker-compose -f /home/ec2-user/docker-compose.yml up -d"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/key.pem")
-      host        = aws_eip.eip.public_ip
-    }
-  }
+	network_configuration {
+		security_groups = [module.vpc.default_security_group_id]
+		subnets = module.vpc.private_subnets
+	}
 }
 
-resource "aws_ecr_repository" "repository" {
-  name = "website-wagtail" 
-
-  image_tag_mutability = "MUTABLE" 
-
-  lifecycle {
-    prevent_destroy = true 
-  }
-
-}
+# * Step 8 - See our application working.
+# * Output the URL of our Application Load Balancer so that we can connect to
+# * our application running inside  ECS once it is up and running.
+output "lb_url" { value = "http://${module.alb.lb_dns_name}" }
