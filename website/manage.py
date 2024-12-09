0b6c3c1a9984a7ccd8f28520de7bb71af782df21
Cody Seibert <cseibert113@gmail.com>
2024-12-09 12:24:04 -0500

    removing create_superuser script and use one built into django

--- website/manage.py
+++ website/manage.py
@@ -2,8 +2,10 @@
 import os
 import sys
 
+env = os.getenv("ENV", "dev")
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"app.settings.{env}")
 
     from django.core.management import execute_from_command_line
 
