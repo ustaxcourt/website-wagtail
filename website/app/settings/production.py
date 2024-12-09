13cebc0cb354aebc7710f3fffdba3fd724045398
Cody Seibert <cseibert113@gmail.com>
2024-12-09 17:12:54 -0500

    adding allowed hosts

--- website/app/settings/production.py
+++ website/app/settings/production.py
@@ -2,7 +2,7 @@ import os
 from .base import *  # noqa: F403
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
