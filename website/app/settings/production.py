234aa2cea98dafeffb10eac553190248501a9a8a
Cody Seibert <cseibert113@gmail.com>
2024-12-09 12:42:10 -0500

    trying to print env to see what is setup on production

--- website/app/settings/production.py
+++ website/app/settings/production.py
@@ -1,9 +1,3 @@
 from .base import *  # noqa: F403
 
 DEBUG = False
-
-try:
-    from .local import *  # noqa: F403
-
-except ImportError:
-    pass
--- website/manage.py
+++ website/manage.py
@@ -4,8 +4,12 @@ import sys
 
 env = os.getenv("ENV", "dev")
 
+print(f"ENV was set to ${env}")
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"app.settings.{env}")
+
+    print(os.envron)
 
     from django.core.management import execute_from_command_line
 
