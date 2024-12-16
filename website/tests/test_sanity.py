014abfb5b5930a94df87067652036b81ee4bf2c9
Sree <guruparan18@gmail.com>
2024-12-16 13:48:44 -0500

    format

--- website/tests/test_sanity.py
+++ website/tests/test_sanity.py
@@ -1,8 +1,8 @@
-
-# This is a trivial test to ensure that pytest & github actions are properly 
-# configured. 
+# This is a trivial test to ensure that pytest & github actions are properly
+# configured.
 
 # TODO: remove this file when we have tests that actually test stuff
+
 
 def test_addition():
     assert 2 + 2 == 4
