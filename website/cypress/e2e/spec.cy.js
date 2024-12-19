bc17c53a567bd2d1ee73e9ebf9e0a1a6de75ee1e
Sree <guruparan18@gmail.com>
2024-12-18 23:14:06 -0500

    revert cypress js and install ts

--- website/cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Admin access', () => {
-  it('Admin is abel to login with username password!', () => {
-    cy.visit('http://localhost:8000/admin')
-    cy.get('input[name=username]').type('admin')
-    cy.get('input[name=password]').type('ustcAdminPW!')
-    cy.get('button[type=submit]').click()
-    cy.url().should('include', '/admin/')
-  })
-})
