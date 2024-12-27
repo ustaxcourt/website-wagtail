396733d2868dd8065cb215b2ce5d0955deb9df0b
Cody Seibert <cseibert113@gmail.com>
2024-12-27 16:11:35 -0500

    trying to fix csrf bug

--- website/app/wsgi.py
+++ website/app/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.dev")
+os.environ.setdefault("DJANGO_CSRF_TRUSTED_ORIGINS", "")
 
 application = get_wsgi_application()
