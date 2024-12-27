d67b30747d2445807fc34b400d4975c2ad7adff7
Cody Seibert <cseibert113@gmail.com>
2024-12-27 15:24:30 -0500

    setting cors

--- website/app/settings/production.py
+++ website/app/settings/production.py
@@ -4,8 +4,7 @@ from .base import *  # noqa: F403
 # TODO: I don't think we'd need this at all?
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-# TODO: this should be configured based on the domain when we get one
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [os.getenv("DOMAIN_NAME")]
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
