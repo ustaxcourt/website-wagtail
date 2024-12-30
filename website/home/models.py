49154c8983f0c3cf9f998f14cea03f52ea855043
Cody Seibert <cseibert113@gmail.com>
2024-12-30 15:53:46 -0500

    Home Page Image + Footer (#30)
    * including the homepage banner image
    
    * adding a footer
    
    * working on the footer
    
    * adding a settings for footer info

--- website/home/models.py
+++ website/home/models.py
@@ -4,6 +4,11 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel
 from modelcluster.fields import ParentalKey
 
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
+
 
 class HomePage(Page):
     intro = RichTextField(blank=True, help_text="Introduction text for the homepage.")
@@ -11,6 +16,19 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("intro"),
         InlinePanel("entries", label="Entries"),
+    ]
+
+
+@register_setting
+class Footer(BaseGenericSetting):
+    technicalQuestions = RichTextField(
+        blank=True, help_text="Content for technical questions."
+    )
+    otherQuestions = RichTextField(blank=True, help_text="Content for other questions.")
+
+    content_panels = Page.content_panels + [
+        FieldPanel("technicalQuestions"),
+        FieldPanel("otherQuestions"),
     ]
 
 
