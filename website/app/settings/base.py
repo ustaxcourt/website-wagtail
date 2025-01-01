44496c33101e7bae4432a3efa1076051537c8af5
Cody Seibert <cseibert113@gmail.com>
2025-01-01 17:12:53 -0500

    try to set storages

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -168,6 +168,16 @@ STORAGES = {
     },
 }
 
+aws_bucket_name = os.getenv("AWS_STORAGE_BUCKET_NAME")
+if aws_bucket_name:
+    STORAGES["default"] = {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    }
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = "us-east-1"
+    AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
 
 # Wagtail settings
 
