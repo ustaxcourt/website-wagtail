07183a24a1389b81fe7dddd015d3e5b27af4dacd
Sree Appachi <guruparan18@gmail.com>
2025-01-09 23:29:21 -0500

    Working version of Google Analytics

--- website/home/models.py
+++ website/home/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django import forms
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel
 from modelcluster.fields import ParentalKey
+from django.conf import settings
 
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
@@ -35,11 +37,16 @@ class Footer(BaseGenericSetting):
 @register_setting
 class GoogleAnalyticsSettings(BaseGenericSetting):
     tracking_id = models.CharField(
-        max_length=20, help_text="Google Analytics Measurement ID (e.g., G-XXXXXXXXXX)"
+        max_length=20,
+        help_text="Google Analytics Measurement ID (e.g., G-1234567890)",
+        default=settings.GOOGLE_ANALYTICS_ID,
     )
 
     panels = [
-        FieldPanel("tracking_id"),
+        FieldPanel(
+            "tracking_id",
+            widget=forms.TextInput(attrs={"value": settings.GOOGLE_ANALYTICS_ID}),
+        ),
     ]
 
 
