68bc585f4787f88e737915fc1a1f974fd834d7be
Sree Appachi <guruparan18@gmail.com>
2025-01-14 10:45:13 -0500

    reset admin password to AWS secrets

--- website/home/management/commands/reset_admin_password.py
+++ website/home/management/commands/reset_admin_password.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-import os
+from home.utils.secrets import get_secret
 
 username = "admin"
 email = "admin@example.com"
-SUPERUSER_PASSWORD = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+SUPERUSER_PASSWORD = get_secret("WAGTAIL_ADMIN_PASSWORD")
 
 
 class Command(BaseCommand):
-    help = "Update superuser admin password to default"
+    help = "Update superuser admin password to default stored in secrets"
 
     def handle(self, *args, **kwargs):
         if User.objects.filter(username=username).exists():
