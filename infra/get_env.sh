2d811f21a6c92f9c90a5c3b5d2cac5b8db24d8e7
Miest-Moore, Miriam V <mmiest-moore@flexion.us>
2025-01-17 09:07:23 -0800

    rename sandboxes

--- infra/get_env.sh
+++ infra/get_env.sh
@@ -5,7 +5,8 @@ ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text 2>/dev/
 case "$ACCOUNT_ID" in
     "640168411491") echo "dev";;
     "202533521356") echo "test";;
-    "221082179478") echo "sandbox";;
-    "047719633139") echo "cody";;
+    "221082179478") echo "sandbox";; # Sree
+    "047719633139") echo "sandbox";; # Cody
+    "354918408861") echo "sandbox";; # Miriam
     *) echo "unknown";;
 esac
