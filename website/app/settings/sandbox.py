cf2cec7f093821e66451257c48b2c95154b01eb4
Sree Appachi <guruparan18@gmail.com>
2025-01-16 23:52:19 -0500

    renamed env

---
+++ website/app/settings/sandbox.py
@@ -0,0 +1,29 @@
+from .base import *  # noqa: F403
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+os.environ.setdefault("SUPERUSER_PASSWORD", "ustcAdminPW!")
+
+try:
+    from .local import *  # noqa: F403
+
+except ImportError:
+    pass
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("DOMAIN_NAME")}']
+# TODO: verify if this is actually needed (read it was needed when using AWS ALB)
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# TODO: verify if these are needed; I had to add / remove a lot of config before I got this all working, so these might not actually be needed
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+DEBUG = False
