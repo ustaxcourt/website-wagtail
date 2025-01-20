6fe50129638b02065309b612dfbb78caf3bb7c59
Cody Seibert <cseibert113@gmail.com>
2025-01-20 12:16:09 -0500

    adding an e2e test to verify the first document link exists and can be downloaded

--- website/cypress/e2e/case_related_forms.cy.ts
+++ website/cypress/e2e/case_related_forms.cy.ts
@@ -2,11 +2,22 @@ import { checkA11y } from "../support/commands"
 
 describe('case related forms page', () => {
   beforeEach(() => {
-    cy.visit('/admin')
+    cy.visit('/case_related_forms')
+    checkA11y()
   })
 
-  it('has required federal banner', () => {
-    cy.get('[data-testid="usa-banner"]').contains('An official website of the United States government')
-  })
 
+  it('verify the documents are displayed and clickable', function() {
+    const documentLink = cy.get('a[data-testid="Application for Order to Take Deposition to Perpetuate Evidence"]');
+    documentLink.should('exist');
+
+    documentLink
+      .then($link => {
+        const href = $link.prop('href');
+        cy.request(href).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.headers['content-type']).to.include('application/pdf');
+        });
+      });
+  });
 })
