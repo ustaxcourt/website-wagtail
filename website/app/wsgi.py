265278336a393a89dd0106bf02edffcb7a37efd8
Cody Seibert <cseibert113@gmail.com>
2025-01-28 11:28:07 -0500

    trying to fix wsgi

--- website/app/wsgi.py
+++ website/app/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.dev")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
