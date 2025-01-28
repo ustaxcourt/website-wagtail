a20640b0cab3fc6f7bb3e96eef19547ac5e310b2
Sree Appachi <guruparan18@gmail.com>
2025-01-27 22:51:50 -0500

    remove hc

--- infra/modules/app/ecs.tf
+++ infra/modules/app/ecs.tf
@@ -80,13 +80,13 @@ resource "aws_ecs_task_definition" "this" {
     image        = docker_registry_image.this.name,
     name         = local.container_name,
     portMappings = [{ containerPort = local.container_port }],
-    healthCheck = {
-      "command"     = ["CMD", "curl", "-f", "http://localhost:${local.container_port}"]
-      "interval"    = 30 # check every 30 seconds
-      "retries"     = 3  # retry 3 times before marking as unhealthy
-      "startPeriod" = 30 # initial delay before starting health checks
-      "timeout"     = 5  # health check timeout in seconds
-    }
+    # healthCheck = {
+    #   "command"     = ["CMD", "curl", "-f", "http://localhost:${local.container_port}"]
+    #   "interval"    = 30 # check every 30 seconds
+    #   "retries"     = 3  # retry 3 times before marking as unhealthy
+    #   "startPeriod" = 30 # initial delay before starting health checks
+    #   "timeout"     = 5  # health check timeout in seconds
+    # }
     logConfiguration = {
       logDriver = "awslogs",
       options = {
