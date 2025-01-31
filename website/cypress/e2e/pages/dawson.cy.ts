8bca3e2f454711013cf2f67dd38c10fada9cd81c
Sree Appachi <guruparan18@gmail.com>
2025-01-30 21:33:34 -0500

    e2e test works

--- website/cypress/e2e/pages/dawson.cy.ts
+++ website/cypress/e2e/pages/dawson.cy.ts
@@ -4,18 +4,18 @@ describe('dawson page', () => {
   it('verify the page load and links are displayed and clickable', function() {
     cy.visit('/dawson/')
 
-    // const documentLink = cy.get('a[data-testid="Volume 161, Numbers 5 and 6"]');
-    // documentLink.should('exist');
+    const logoLink = cy.get('a[data-testid="dawson-logo"]');
+    logoLink.should('exist');
 
     checkA11y()
 
-    // documentLink
-    //   .then($link => {
-    //     const href = $link.prop('href');
-    //     cy.request(href).then((response) => {
-    //       expect(response.status).to.eq(200);
-    //       expect(response.headers['content-type']).to.include('application/pdf');
-    //     });
-    //   });
+    logoLink
+      .then($link => {
+        const href = $link.prop('href');
+        cy.request(href).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.headers['content-type']).to.exist;
+        });
+      });
   });
 })
