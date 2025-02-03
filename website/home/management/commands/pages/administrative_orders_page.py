1b7c1fd88d14a71fa7be73e37f07210d98af75bf
Sree Appachi <guruparan18@gmail.com>
2025-02-03 18:48:10 -0500

    adding PDFs

--- website/home/management/commands/pages/administrative_orders_page.py
+++ website/home/management/commands/pages/administrative_orders_page.py
@@ -5,17 +5,18 @@ from home.management.commands.pages.page_initializer import PageInitializer
 
 # Example PDF data
 ADMIN_ORDERS_DATA = [
-    "Administrative_Order_2020-01.pdf",
-    "Administrative_Order_2020-02.pdf",
-    "Administrative_Order_2020-03.pdf",
-    "Administrative_Order_2020-04.pdf",
-    "Administrative_Order_2020-05.pdf",
-    "Administrative_Order_2021-02_REPEALED.pdf",
-    "Administrative_Order_2021-03_REPEALED.pdf",
-    "Administrative_Order_2022-01_Repealed.pdf",
-    "Administrative_Order_2023-01_Repealed.pdf",
-    "Administrative_Order_2023-02.pdf",
-    "Administrative_Order_2024-01.pdf",
+    {"title": "Administrative Order 2020-01 - Postponement of 2020 Nonattorney Examination", "name": "Administrative_Order_2020-01.pdf"},
+    {"title": "Administrative Order 2020-02 - Remote Court Proceedings During COVID-19 Pandemic", "name": "Administrative_Order_2020-02.pdf"},
+    {"title": "Administrative Order 2020-03 - Limited Entry of Appearance Procedures", "name": "Administrative_Order_2020-03.pdf"},
+    {"title": "Administrative Order 2020-04 - Answer Filing Deadline During the Transition to a New Case Management System", "name": "Administrative_Order_2020-04.pdf"},
+    {"title": "Administrative Order 2020-05 - Digital Image Signatures on Paper Copies During the Transition to the New Case Management System", "name": "Administrative_Order_2020-05.pdf"},
+    {"title": "Administrative Order 2021-01 - Policies for Remote (Virtual) Proceedings", "name": "Administrative_Order_2021-01.pdf"},
+    {"title": "Administrative Order 2021-02 - Washington, DC Courthouse Access (REPEALED)", "name": "Administrative_Order_2021-02_REPEALED.pdf"},
+    {"title": "Administrative Order 2021-03 - Protocol for Court Personnel and Contractors Entry into Washington, D.C. Courthouse (REPEALED)", "name": "Administrative_Order_2021-03_REPEALED.pdf"},
+    {"title": "Administrative Order 2022-01 - Protocols for Entry into the Washington, D.C. Courthouse and In-Person Court Proceedings in Other Locations (REPEALED)", "name": "Administrative_Order_2022-01_Repealed.pdf"},
+    {"title": "Administrative Order 2023-01 - Protocols for Entry into the Washington, D.C. Courthouse and In-Person Court Proceedings in Other Locations (REPEALED)", "name": "Administrative_Order_2023-01_Repealed.pdf"},
+    {"title": "Administrative Order 2023-02 - Expanding Remote Electronic Access to Certain Court Documents", "name": "Administrative_Order_2023-02.pdf"},
+    {"title": "Administrative Order 2024-01 - Protocols for Entry into the Washington, D.C. Courthouse and In-Person Court Proceedings in Other Locations", "name": "Administrative_Order_2024-01.pdf"}
 ]
 
 
