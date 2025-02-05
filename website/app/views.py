c922db10d9997b1afba0d0344a91c44ffa94793d
Joseph Tooley <jtooley@flexion.us>
2025-02-05 10:59:45 -0600

    remove unneccessary views

--- website/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def redirect_view(request):
-    external_url = request.GET.get("url", "/")
-    return render(request, "redirect_page.html", {"external_url": external_url})
