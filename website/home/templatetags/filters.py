fe7875890e9bf95e358d1db53dc37aab359e6904
Sree Appachi <guruparan18@gmail.com>
2025-02-24 12:23:46 -0500

    columns

--- website/home/templatetags/filters.py
+++ website/home/templatetags/filters.py
@@ -8,3 +8,8 @@ def strip_trailing_slash(value):
     if value.endswith("/"):
         return value[:-1]
     return value
+
+
+@register.filter
+def column_width(column_count):
+    return 12 // column_count
