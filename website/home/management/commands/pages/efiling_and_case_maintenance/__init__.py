a58a41fb7d67be6e3fabf62a784426bc7a119d31
Joseph Tooley <jtooley@flexion.us>
2025-03-03 15:41:33 -0600

    Fill in forms instruction page

--- website/home/management/commands/pages/efiling_and_case_maintenance/__init__.py
+++ website/home/management/commands/pages/efiling_and_case_maintenance/__init__.py
@@ -1,10 +1,12 @@
 from .case_related_forms_page import CaseRelatedFormPageInitializer
 from .dawson_page import DawsonPageInitializer
 from .dawson_search_page import DawsonSearchPageInitializer
+from .fill_in_form_instructions_page import FillInFormsInstructionsPageInitializer
 
 
 efiling_and_case_maintenance_pages_to_initialize = [
     DawsonSearchPageInitializer,
     DawsonPageInitializer,
     CaseRelatedFormPageInitializer,
+    FillInFormsInstructionsPageInitializer,
 ]
