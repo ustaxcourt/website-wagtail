02d815a0a96e47698c68f343d8d3f1c81793f4b5
Sree Appachi <guruparan18@gmail.com>
2025-03-10 12:02:50 -0400

    Add snippet to enhanced page

---
+++ website/home/management/commands/snippets/clinics_contact_details_snippet.py
@@ -0,0 +1,21 @@
+from home.models import CommonText
+
+
+class ClinicsContactDetailsSnippetInitializer:
+    def __init__(self, logger):
+        self.logger = logger
+        self.name = "Clinics Contact Details"
+
+    def create(self):
+        if CommonText.objects.filter(name=self.name).exists():
+            self.logger.write(f"{self.name} already exists.")
+            return
+
+        self.logger.write(f"Creating the snippet {self.name}.")
+
+        clinics_contact_details = CommonText(
+            name=self.name,
+            text="""Please <a href="mailto:litc@ustaxcourt.gov" title="email: litc@ustaxcourt.gov">contact us</a> with any questions concerning the Court’s program or requirements, or call <a href="tel:202-521-3366" title="call: 202-521-3366">202-521-3366</a>.""",
+        )
+
+        clinics_contact_details.save()
