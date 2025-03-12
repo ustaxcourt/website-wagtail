b520a6a65947a748abe0314a7810c409789ce451
Sree Appachi <guruparan18@gmail.com>
2025-03-12 16:48:22 -0400

    revert image

--- website/app/urls.py
+++ website/app/urls.py
@@ -25,7 +25,6 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
-        image_root=settings.MEDIA_ROOT,
     )
 
 urlpatterns = urlpatterns + [
