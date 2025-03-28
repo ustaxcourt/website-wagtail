bc434781817f4a7a57345ba4c0b9fa97a1b1fcfb
Cody Seibert <cseibert113@gmail.com>
2025-03-28 15:13:09 -0400

    add a context process to display the sha

---
+++ website/app/context_processors.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+
+
+def build_info(request):
+    return {"build_sha": settings.GITHUB_SHA}
--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "wagtail.contrib.settings.context_processors.settings",
+                "app.context_processors.build_info",
             ],
         },
     },
