2ca072d63a478b4bbbe47d115e4f6c492d5f1a53
Sree Appachi <guruparan18@gmail.com>
2025-04-03 12:31:12 -0400

    making judge recruiting displays

---
+++ website/home/management/commands/pages/about_the_court/judges_recruiting_page.py
@@ -0,0 +1,57 @@
+from wagtail.models import Page
+from home.models import JudgesRecruiting
+from home.management.commands.pages.page_initializer import PageInitializer
+
+
+class JudgesRecruitingPageInitializer(PageInitializer):
+    def __init__(self, logger):
+        super().__init__(logger)
+
+    def create(self):
+        # Find the Employment page instead of home page
+        try:
+            employment_page = Page.objects.get(slug="employment")
+        except Page.DoesNotExist:
+            self.logger.write(
+                "Error: Employment page does not exist. Please create it first."
+            )
+            return
+
+        slug = "judges-recruiting"
+        title = "Judges Currently Recruiting"
+
+        if Page.objects.filter(slug=slug).exists():
+            self.logger.write(f"- {title} page already exists.")
+            return
+
+        self.logger.write(f"Creating the '{title}' page.")
+
+        # Add as child of employment_page instead of home_page
+        employment_page.add_child(
+            instance=JudgesRecruiting(
+                title=title,
+                judges_recruiting=[
+                    {
+                        "type": "message",
+                        "value": "No judges are currently recruiting. Questions concerning the application process may be directed to the Court's Office of Human Resources at (202) 521-4700.",
+                    },
+                    {
+                        "type": "judge",
+                        "value": [
+                            {
+                                "judge_name": "Judge John Doe",
+                                "description": "Currently recruiting for the position of Judge.",
+                                "apply_to_email": "(202) 555-0123",
+                                "display_from": "2025-10-01",
+                                "display_to": "2025-12-31",
+                            },
+                        ],
+                    },
+                ],
+                slug=slug,
+                seo_title=title,
+                search_description=f"{title} Announcements for the United States Tax Court",
+            )
+        )
+
+        self.logger.write(f"Successfully created the '{title}' page.")
