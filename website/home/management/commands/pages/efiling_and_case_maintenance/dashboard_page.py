c1c71c7893e419c686cf0ff63a2199ed3e08b854
Cody Seibert <cseibert113@gmail.com>
2025-04-04 17:25:10 -0400

    adding new block for ul / ol

--- website/home/management/commands/pages/efiling_and_case_maintenance/dashboard_page.py
+++ website/home/management/commands/pages/efiling_and_case_maintenance/dashboard_page.py
@@ -1,5 +1,5 @@
 from wagtail.models import Page
-from home.models import EnhancedRawHTMLPage
+from home.models import EnhancedStandardPage
 from home.management.commands.pages.page_initializer import PageInitializer
 
 
@@ -48,7 +48,7 @@ class DashboardPageInitializer(PageInitializer):
                 }
 
         new_page = home_page.add_child(
-            instance=EnhancedRawHTMLPage(
+            instance=EnhancedStandardPage(
                 title=title,
                 slug=slug,
                 seo_title=title,
@@ -56,55 +56,71 @@ class DashboardPageInitializer(PageInitializer):
                     {
                         "type": "paragraph",
                         "value": "Your dashboard is your main landing page once you are signed in to DAWSON.",
-                    }
-                ],
-                search_description="View and manage your DAWSON dashboard",
-                raw_html_body=[
+                    },
+                    {"type": "h2", "value": "Viewing Your Dashboard"},
                     {
-                        "type": "raw_html",
-                        "value": """
-<div id='dashboard-page'>
-<h2>Viewing Your Dashboard</h2>
-
-<ol>
-    <li>Before you file a Petition with the Court, your dashboard will provide information and links to help you with the Petition filing process:</li>
-</ol>
-
-<img src="{welcome_image_url}" alt="Welcome to DAWSON dashboard view">
-
-<ol start="2">
-    <li>Helpful Links</li>
-    <ul>
-        <li><strong><a href="/petitioners-start">How to eFile a Petition</a></strong></li>
-        <li><strong><a href="/dpt_cities.html">Find a Court Location</a></strong></li>
-        <li><strong><a href="/case-related-forms">U.S. Tax Court Forms</a></strong></li>
-        <li><strong><a href="/clinics">Free Taxpayer Assistance</a></strong></li>
-    </ul>
-</ol>
-
-<ol start="3">
-    <li>Once you have <strong><a href="/petitioners-start">filed a Petition</a></strong>, you can view all of your cases (open and closed) on your dashboard:</li>
-</ol>
-
-<img src="{cases_image_url}" alt="Open cases dashboard view">
-
-<h3>Tips & Tricks</h3>
-
-<ul>
-    <li>To return to your dashboard from anywhere within DAWSON, click <strong>My Cases</strong>.</li>
-    <li>Open cases and closed cases are displayed on separate tabs. The number of cases for each is displayed in parentheses.</li>
-    <li>The default display is 20 cases. To view more, click the <strong>Load More</strong> button.</li>
-    <li>It is typical for a self-represented (pro se) petitioner to have just one case.</li>
-</ul>
-</div>
-""".format(
-                            welcome_image_url=uploaded_images["welcome-to-dawson.jpg"][
-                                "url"
+                        "type": "list",
+                        "value": {
+                            "list_type": "ordered",
+                            "items": [
+                                {
+                                    "text": "Before you file a Petition with the Court, your dashboard will provide information and links to help you with the Petition filing process:",
+                                    "image": uploaded_images["welcome-to-dawson.jpg"][
+                                        "id"
+                                    ],
+                                },
+                                {
+                                    "text": "Helpful Links",
+                                    "nested_list": [
+                                        {
+                                            "list_type": "unordered",
+                                            "items": [
+                                                {
+                                                    "text": "<a href='/petitioners-start'>How to eFile a Petition</a>",
+                                                },
+                                                {
+                                                    "text": "<a href='/dpt_cities.html'>Find a Court Location</a>",
+                                                },
+                                                {
+                                                    "text": "<a href='/case-related-forms'>U.S. Tax Court Forms</a>",
+                                                },
+                                                {
+                                                    "text": "<a href='/clinics'>Free Taxpayer Assistance</a>",
+                                                },
+                                            ],
+                                        }
+                                    ],
+                                },
+                                {
+                                    "text": "Once you have filed a Petition, you can view all of your cases (open and closed) on your dashboard:",
+                                    "image": uploaded_images["open-cases.jpg"]["id"],
+                                },
                             ],
-                            cases_image_url=uploaded_images["open-cases.jpg"]["url"],
-                        ),
+                        },
+                    },
+                    {"type": "h2", "value": "Tips & Tricks"},
+                    {
+                        "type": "list",
+                        "value": {
+                            "list_type": "ordered",
+                            "items": [
+                                {
+                                    "text": "To return to your dashboard from anywhere within DAWSON, click <strong>My Cases</strong>.",
+                                },
+                                {
+                                    "text": "Open cases and closed cases are displayed on separate tabs. The number of cases for each is displayed in parentheses.",
+                                },
+                                {
+                                    "text": "The default display is 20 cases. To view more, click the <strong>Load More</strong> button.",
+                                },
+                                {
+                                    "text": "It is typical for a self-represented (pro se) petitioner to have just one case.",
+                                },
+                            ],
+                        },
                     },
                 ],
+                search_description="View and manage your DAWSON dashboard",
             )
         )
 
