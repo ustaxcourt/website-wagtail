996e7ab8904fae5379e3d24236a5a725b421c137
Sree Appachi <guruparan18@gmail.com>
2025-04-07 15:08:27 -0400

    clear typo

--- website/home/management/commands/pages/efiling_and_case_maintenance/definitions_page.py
+++ website/home/management/commands/pages/efiling_and_case_maintenance/definitions_page.py
@@ -31,38 +31,38 @@ class DawsonFaqsDefinitionsPageInitializer(PageInitializer):
         }
 
         page_body = (
-            '<p data-block-key="gxnu3">“Designated Service Person” means the'
+            '<p>“Designated Service Person” means the'
             " practitioner designated to receive service of documents in a case. The"
             " first counsel of record is generally the Designated Service Person, see"
             ' <a linktype="document"'
             f' id="{definition_docs["Rule-21_Amended_03202023.pdf"].id}"><b>Rule'
             " 21(b)(2)</b></a>. The ability to designate an additional service person"
-            ' in DAWSON is coming soon.<br/></p><p data-block-key="hctv">“Document”'
+            ' in DAWSON is coming soon.</p><p>“Document”'
             " means any written matter filed by or with the Court including, but not"
             " limited to motions, pleadings, applications, petitions, notices,"
             " declarations, affidavits, exhibits, briefs, memoranda of law, orders,"
-            ' and deposition transcripts.<br/></p><p data-block-key="b8ojv">“eLodged”'
+            ' and deposition transcripts.</p><p>“eLodged”'
             " refers to any document that is electronically submitted to the Court"
             " with a motion for leave through DAWSON and that is not automatically"
-            ' filed.<br/></p><p data-block-key="408m7">“Intervenor” is a third party'
+            ' filed.</p><p>“Intervenor” is a third party'
             " who has an interest in the outcome of the case. The most common example"
             " is the spouse or former spouse of a petitioner seeking innocent spouse"
             " relief. “Participant” is a partner who elects to participate in a"
             " partnership action by filing a notice of election to participate under"
             f' <a linktype="document" id="{definition_docs["Rule-245.pdf"].id}"><b>Rule'
-            ' 245</b></a>.<br/></p><p data-block-key="9sv9q">“Party”, for purposes of'
+            ' 245</b></a>.</p><p>“Party”, for purposes of'
             " electronic access, means either petitioner(s) or respondent"
-            ' (IRS).<br/></p><p data-block-key="5i9et">“PDF” means Portable Document'
+            ' (IRS).</p><p>“PDF” means Portable Document'
             " Format. Documents in PDF may be opened in Adobe Reader or an equivalent"
             " viewer. Adobe Reader may be downloaded free of charge from the Adobe"
             " website (www.adobe.com). Electronic documents may be converted to PDF"
             " through a word processor, third party PDF creation software such as"
             " Adobe Acrobat, or online PDF creation services from Adobe (<a"
             ' href="https://createpdf.adobe.com/">https://createpdf.adobe.com/</a>) and'
-            " others. Documents in paper form may be scanned into PDF.<br/></p><p"
-            ' data-block-key="8bdcg">“Pro Se” means a petitioner who represents'
+            " others. Documents in paper form may be scanned into PDF.</p>"
+            '<p>“Pro Se” means a petitioner who represents'
             " themselves without a lawyer or an entity appearing through an authorized"
-            ' fiduciary or officer.<br/></p><p data-block-key="8h41i">For additional'
+            ' fiduciary or officer.</p><p>For additional'
             ' terms, please visit the <a href="/petitioners-glossary" title="Guidance for Petitioners: Glossary">glossary</a>.</p>'
         )
 
