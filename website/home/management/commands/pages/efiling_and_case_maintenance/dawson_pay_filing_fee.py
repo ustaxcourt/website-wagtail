4ad06ce585c6b2225ad28248f71b66d5c8144198
Sree Appachi <guruparan18@gmail.com>
2025-04-09 19:21:35 -0400

    page load works

--- website/home/management/commands/pages/efiling_and_case_maintenance/dawson_pay_filing_fee.py
+++ website/home/management/commands/pages/efiling_and_case_maintenance/dawson_pay_filing_fee.py
@@ -3,7 +3,7 @@ from home.management.commands.pages.page_initializer import PageInitializer
 from home.models import EnhancedStandardPage
 
 docs = {
-    "dawson_pay_filing_fee.py": "",
+    "dawson_pay_filing_fee.pdf": "",
     "Application_for_Waiver_of_Filing_Fee.pdf": "",
 }
 
@@ -44,7 +44,7 @@ class DawsonPayFilingFeeInitializer(PageInitializer):
 
         for image_name in images.keys():
             image_uploaded = self.load_image_from_images_dir(
-                "dawson", image_name, images[image_name]
+                subdirectory="dawson", filename=image_name
             )
             images[image_name] = image_uploaded
 
@@ -61,6 +61,67 @@ class DawsonPayFilingFeeInitializer(PageInitializer):
                         "value": f"""
                             Filing fees are required to submit a petition. The Court’s filing fee is $60 and may be paid online, by mail, or in person. The fee may be waived by filing an <a href="{docs['Application_for_Waiver_of_Filing_Fee.pdf'].file.url}">Application for Waiver of Filing Fee</a>. Your petition must be processed by the Court before the Application for Waiver of Filing fee can be filed electronically.
                         """,
+                    },
+                    {
+                        "type": "hr",
+                        "value": True,
+                    },
+                    {
+                        "type": "h2",
+                        "value": "How to Pay the Filing Fee Online",
+                    },
+                    {
+                        "type": "list",
+                        "value": {
+                            "list_type": "ordered",
+                            "items": [
+                                {
+                                    "text": "The Court uses <a href='https://pay.gov' title='Pay.gov'>pay.gov</a> for processing payments. To pay the filing fee online, <strong>complete the</strong> <a href='https://www.pay.gov/public/form/start/60485840' title='US Tax Court Fees - Petitions'>Tax Court Petitions Fees form</a>.",
+                                    "image": images["dawson_pay_gov.jpg"].id,
+                                },
+                                {
+                                    "text": "Click <strong>Continue to the Form </strong>."
+                                },
+                                {
+                                    "text": "<strong>Fill out</strong> personal details, including your U.S. Tax Court Docket Number(s). Docket Number(s) should be entered in xxxxx-xx format (e.g., 12345-20).",
+                                    "nested_list": [
+                                        {
+                                            "list_type": "unordered",
+                                            "items": [
+                                                {
+                                                    "text": "If you filed electronically with DAWSON, the Court’s electronic case management system, you received your Docket Number on the confirmation screen. It is also listed by your case on your dashboard."
+                                                },
+                                                {
+                                                    "text": "If you submitted your petition by mail, you received your Docket Number in the mail on the Notice of Receipt of Petition document.",
+                                                    "image": images[
+                                                        "dawson_us_tax_court_fees_petitions_agency_form.png"
+                                                    ].id,
+                                                },
+                                            ],
+                                        }
+                                    ],
+                                },
+                                {"text": "Click <strong>Continue</strong>."},
+                                {
+                                    "text": "Confirm how you would like to pay.",
+                                    "image": images[
+                                        "dawson_us_tax_court_fees_petitions_enter_payment_info.png"
+                                    ].id,
+                                },
+                                {
+                                    "text": "Click <strong>Next</strong>.",
+                                },
+                                {
+                                    "text": "Enter payment information.",
+                                },
+                                {
+                                    "text": "Click <strong>Review and Submit Payment</strong>.",
+                                },
+                                {
+                                    "text": "<strong>Print</strong> Confirmation Page.",
+                                },
+                            ],
+                        },
                     },
                     {
                         "type": "hr",
