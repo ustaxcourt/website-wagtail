8a0ea4363d79b63ae466da0cd37780c10380594a
Sree Appachi <guruparan18@gmail.com>
2025-04-15 21:40:12 -0400

    remove some of the unused pages

--- website/home/management/commands/pages/orders_and_opinions/__init__.py
+++ website/home/management/commands/pages/orders_and_opinions/__init__.py
@@ -1,14 +1,10 @@
 from .citation_style_manual_page import CitationStyleManualPageInitializer
 from .pamphlets_page import PamphletsPageInitializer
 from .search_page import SearchPageInitializer
-from .todays_opinions import TodaysOpinionsPageInitializer
-from .todays_orders import TodaysOrdersPageInitializer
 from .transcripts_and_copies_page import TranscriptsAndCopiesPageInitializer
 
 
 orders_and_opinions_pages_to_initialize = [
-    TodaysOpinionsPageInitializer,
-    TodaysOrdersPageInitializer,
     SearchPageInitializer,
     CitationStyleManualPageInitializer,
     TranscriptsAndCopiesPageInitializer,
