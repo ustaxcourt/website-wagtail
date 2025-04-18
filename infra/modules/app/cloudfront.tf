54841b12c72b3d600b0620519dc0d00190aab4fb
Cody Seibert <cseibert113@gmail.com>
2025-04-18 12:33:43 -0400

    trying to fix domain

--- infra/modules/app/cloudfront.tf
+++ infra/modules/app/cloudfront.tf
@@ -41,7 +41,7 @@ resource "aws_cloudfront_distribution" "files_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.main.arn
+    acm_certificate_arn      = aws_acm_certificate.files_cert.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
