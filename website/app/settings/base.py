cd16d665af86b5ec43c1c351bb3e4263994694c8
Cody Seibert <cseibert113@gmail.com>
2025-04-21 10:20:15 -0400

    trying to fix domain

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -214,7 +214,7 @@ if aws_bucket_name:
     }
     AWS_STORAGE_BUCKET_NAME = aws_bucket_name
     AWS_S3_REGION_NAME = "us-east-1"
-    # Use CloudFront domain for documents
+    # Use CloudFront domain for documentss
     AWS_S3_CUSTOM_DOMAIN = os.getenv("DOMAIN_NAME")
     MEDIA_URL = "https://%s/files/" % AWS_S3_CUSTOM_DOMAIN
     AWS_DEFAULT_ACL = None
