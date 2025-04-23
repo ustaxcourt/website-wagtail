9d3254ebf1cc4a8224741e228b58b78e9f4fc567
Sree Appachi <guruparan18@gmail.com>
2025-04-23 09:18:53 -0400

    cleanup settings

--- website/app/settings/dev.py
+++ website/app/settings/dev.py
@@ -1,10 +1,6 @@
 from .base import *  # noqa: F403
 import os
 
-# SECURITY WARNING: define the correct hosts in production!
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 os.environ.setdefault("DJANGO_SUPERUSER_PASSWORD", "ustcAdminPW!")
 
 try:
@@ -12,13 +8,6 @@ try:
 
 except ImportError:
     pass
-
-# TODO: verify if this is actually needed (read it was needed when using AWS ALB)
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-# TODO: verify if these are needed; I had to add / remove a lot of config before I got this all working, so these might not actually be needed
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
 
 DEBUG = False
 BASE_URL = f'https://{os.getenv("DOMAIN_NAME")}'
