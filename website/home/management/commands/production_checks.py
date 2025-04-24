fa100658bcf643e39714b205c0b4f2b4e17b786f
Sree Appachi <guruparan18@gmail.com>
2025-04-23 23:10:56 -0400

    more checks

--- website/home/management/commands/production_checks.py
+++ website/home/management/commands/production_checks.py
@@ -9,24 +9,38 @@ class Command(BaseCommand):
         RAISE_ERROR = False
         # Check if DEBUG is False
         if settings.DEBUG:
-            self.stdout.write(self.style.ERROR("DEBUG is set to True!"))
+            self.stdout.write(self.style.ERROR("DEBUG is set to True."))
             RAISE_ERROR = True
         else:
             self.stdout.write(self.style.SUCCESS("DEBUG is set to False."))
 
         # Check if SECRET_KEY is set
         if not settings.SECRET_KEY:
-            self.stdout.write(self.style.ERROR("SECRET_KEY is not set!"))
+            self.stdout.write(self.style.ERROR("SECRET_KEY is not set."))
             RAISE_ERROR = True
         else:
             self.stdout.write(self.style.SUCCESS("SECRET_KEY is set."))
 
         # Check if ALLOWED_HOSTS is set
         if not settings.ALLOWED_HOSTS:
-            self.stdout.write(self.style.ERROR("ALLOWED_HOSTS is not set!"))
+            self.stdout.write(self.style.ERROR("ALLOWED_HOSTS is not set."))
             RAISE_ERROR = True
         else:
             self.stdout.write(self.style.SUCCESS("ALLOWED_HOSTS is set."))
+
+        if not settings.CSRF_COOKIE_SECURE:
+            self.stdout.write(self.style.ERROR("CSRF_COOKIE_SECURE is not set."))
+            RAISE_ERROR = True
+        else:
+            self.stdout.write(self.style.SUCCESS("CSRF_COOKIE_SECURE is set to True."))
+
+        if not settings.SESSION_COOKIE_SECURE:
+            self.stdout.write(self.style.ERROR("SESSION_COOKIE_SECURE is not set."))
+            RAISE_ERROR = True
+        else:
+            self.stdout.write(
+                self.style.SUCCESS("SESSION_COOKIE_SECURE is set to True.")
+            )
 
         # Check if raising errors is enabled
         if RAISE_ERROR:
