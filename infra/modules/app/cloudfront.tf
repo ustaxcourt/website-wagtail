d4d9208c52901f476ffd8f5f0fca5d29b2b63224
Cody Seibert <cseibert113@gmail.com>
2025-04-24 15:39:44 -0400

    try to add logs

--- infra/modules/app/cloudfront.tf
+++ infra/modules/app/cloudfront.tf
@@ -107,9 +107,23 @@ resource "aws_s3_bucket" "cloudfront_logs" {
   bucket = "${var.environment}-ustc-website-cloudfront-logs"
 }
 
-resource "aws_s3_bucket_acl" "cloudfront_logs" {
+# Enable bucket ownership controls
+resource "aws_s3_bucket_ownership_controls" "cloudfront_logs" {
   bucket = aws_s3_bucket.cloudfront_logs.id
-  acl    = "private"
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Set bucket public access block
+resource "aws_s3_bucket_public_access_block" "cloudfront_logs" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "cloudfront_logs" {
