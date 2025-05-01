63b94696b90a998e0c40da372a65928cfee2315a
Tejha Bollu <tbollu@flexion.us>
2025-05-01 15:36:31 -0400

    Build NEW COMBINED page/functionality and dropdown: Home and Press Releases & News

--- website/home/management/commands/pages/home_page.py
+++ website/home/management/commands/pages/home_page.py
@@ -1,6 +1,7 @@
 from wagtail.models import Page, Site
 from home.models import HomePage, HomePageEntry, HomePageImage
 from home.management.commands.pages.page_initializer import PageInitializer
+from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
@@ -78,24 +79,54 @@ class HomePageInitializer(PageInitializer):
 
         HomePageEntry.objects.create(
             homepage=homepage,
-            title="",
+            title="Remote Proceedings Info",
             body=(
                 'Guidance on remote (virtual) proceedings and example videos of various procedures in a virtual courtroom can be found <a target="_blank" href="https://ustaxcourt.gov/zoomgov.html">here.</a>'
             ),
+            start_date=datetime(2025, 4, 1).date(),
+            end_date=datetime(2025, 4, 10).date(),
+            persist_to_press_releases=True,
+        )
+        HomePageEntry.objects.create(
+            homepage=homepage,
+            title="Closed for Holidays",
+            body=(
+                "In addition to observing the Christmas Day holiday on Wednesday, December 25, 2024, the Court will be closed on Tuesday, December 24, 2024. DAWSON will remain available for electronic access and electronic filing."
+            ),
+            start_date=datetime(2025, 4, 1).date(),
+            end_date=datetime(2025, 4, 25).date(),
+            persist_to_press_releases=True,
+        )
+
+        HomePageEntry.objects.create(
+            homepage=homepage,
+            title="",
+            body=(
+                'Guidance on remote (virtual) proceedings and example videos of various procedures in a virtual courtroom can be found <a target="_blank" href="/zoomgov">here.</a>'
+            ),
+            start_date=datetime(2025, 4, 14).date(),
+            end_date=None,
+            persist_to_press_releases=True,
         )
         HomePageEntry.objects.create(
             homepage=homepage,
             title="The Tax Court announced that Chief Special Trial Judge Lewis R. Carluzzo has decided to step down as Chief Special Trial Judge, effective May 2, 2025, and that Special Trial Judge Zachary S. Fried has been named Chief Special Trial Judge, effective May 3, 2025.",
             body=(
-                "See the <a target='_blank' href='/press-release'>Press Release</a>."
+                "See the <a target='_blank' href='/press-releases'>Press Release</a>."
             ),
+            start_date=datetime(2025, 4, 14).date(),
+            end_date=None,
+            persist_to_press_releases=True,
         )
         HomePageEntry.objects.create(
             homepage=homepage,
             title="Tax Court Judge Julian I. Jacobs passed away on April 5, 2025.",
             body=(
-                "See the <a target='_blank' href='/press-release'>Press Release</a>."
+                "See the <a target='_blank' href='/press-releases'>Press Release</a>."
             ),
+            start_date=datetime(2025, 4, 14).date(),
+            end_date=None,
+            persist_to_press_releases=True,
         )
         HomePageEntry.objects.create(
             homepage=homepage,
@@ -111,6 +142,9 @@ class HomePageInitializer(PageInitializer):
                 "<p>The IRS posts current <a href='https://www.irs.gov/newsroom/tax-scams-consumer-alerts' target='_blank'>warnings and alerts</a> about all types of tax scams on its website (including information about how to report tax scams). In addition, you may file a consumer complaint about a tax scam with the <a href='https://www.ftc.gov' target='_blank'>Federal Trade Commission</a> (FTC) or the <a href='https://www.fbi.gov' target='_blank'>Federal Bureau of Investigation</a> (FBI). These websites are maintained by the FTC and FBI — government agencies that are unrelated to the Tax Court.</p>"
                 "<p>If you would like to verify that the communication you received is really from the Tax Court, please call the Court at (202) 521-0700.</p>"
             ),
+            start_date=None,
+            end_date=None,
+            persist_to_press_releases=True,
         )
 
         logger.info("Successfully created the new Home page.")
