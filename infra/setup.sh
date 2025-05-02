cf209e6e323c7b54a34ce46516140c41f4715516
Cody Seibert <cseibert113@gmail.com>
2025-05-02 12:04:04 -0400

    remove unecessary line

--- infra/setup.sh
+++ infra/setup.sh
@@ -27,7 +27,6 @@ export TF_VAR_bastion_public_key=$BASTION_PUBLIC_KEY
 export TF_VAR_environment=$ENVIRONMENT
 export TF_VAR_secret_key=$SECRET_KEY
 export TF_VAR_domain_name=$DOMAIN_NAME
-export TF_VAR_alias_name=$ALIAS_NAME
 export TF_VAR_github_sha=$GITHUB_SHA
 export TF_VAR_social_auth_azuread_tenant_oauth2_key=$SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_KEY
 export TF_VAR_social_auth_azuread_tenant_oauth2_secret=$SOCIAL_AUTH_AZUREAD_TENANT_OAUTH2_SECRET
