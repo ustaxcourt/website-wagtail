cb5264e254c1869c6bf84648f0110672df98f966
Sree Appachi <guruparan18@gmail.com>
2025-05-06 16:58:08 -0400

    debug 2

--- website/home/management/commands/pages/home_page.py
+++ website/home/management/commands/pages/home_page.py
@@ -75,8 +75,6 @@ class HomePageInitializer(PageInitializer):
         for document in home_docs.keys():
             uploaded_document = self.load_document_from_documents_dir(None, document)
             home_docs[document] = uploaded_document.file.id
-            print(uploaded_document.file.url)
-            print(uploaded_document.file.path)
             print(home_docs)
 
         # delete the wagtail generated page (it doesn't have the mixin)
--- website/home/management/commands/pages/page_initializer.py
+++ website/home/management/commands/pages/page_initializer.py
@@ -95,6 +95,7 @@ class PageInitializer(ABC):
         if Document.objects.filter(title=title).exists():
             logger.info(f"Document with title '{title}' already exists.")
             document = Document.objects.get(title=title)
+            print(document.file.url)
             return Document.objects.get(title=title)
 
         collection_obj = self.get_or_create_collection_with_login_restriction(
