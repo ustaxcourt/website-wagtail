d672f67a69130f573e1a98e5f7454e97a8054eef
Sree Appachi <guruparan18@gmail.com>
2025-05-06 19:38:24 -0400

    revert home

--- website/home/management/commands/pages/home_page.py
+++ website/home/management/commands/pages/home_page.py
@@ -42,8 +42,7 @@ class HomePageInitializer(PageInitializer):
 
         if HomePage.objects.filter(title=title).exists():
             logger.info(f"- {title} page already exists.")
-            HomePage.objects.filter(title=title).delete()
-            # return
+            return
 
         homepage = HomePage(
             title=title,
@@ -64,7 +63,7 @@ class HomePageInitializer(PageInitializer):
             homepage.images = loaded_images
 
         root.add_child(instance=homepage)
-        # homepage.save_revision().publish()
+        homepage.save_revision().publish()
 
         site = Site.objects.filter(is_default_site=True).first()
         if site:
@@ -75,7 +74,6 @@ class HomePageInitializer(PageInitializer):
         for document in home_docs.keys():
             uploaded_document = self.load_document_from_documents_dir(None, document)
             home_docs[document] = uploaded_document.file.url
-            print(home_docs)
 
         # delete the wagtail generated page (it doesn't have the mixin)
         wagtailHome = Page.objects.filter(
@@ -123,7 +121,7 @@ class HomePageInitializer(PageInitializer):
             homepage=homepage,
             title="Tax Court disciplinary matters.",
             body=(
-                f"""See the <a href="./{home_docs["04292025.pdf"]}" target="_blank">Press Release</a>."""
+                f"""See the <a href="{home_docs["04292025.pdf"]}" target="_blank">Press Release</a>."""
             ),
             start_date=datetime(2025, 4, 29).date(),
             end_date=None,
@@ -133,7 +131,7 @@ class HomePageInitializer(PageInitializer):
             homepage=homepage,
             title="The Tax Court announced that Chief Special Trial Judge Lewis R. Carluzzo has decided to step down as Chief Special Trial Judge, effective May 2, 2025, and that Special Trial Judge Zachary S. Fried has been named Chief Special Trial Judge, effective May 3, 2025.",
             body=(
-                f"""See the <a href="./{home_docs["04162025.pdf"]}" target="_blank">Press Release</a>."""
+                f"""See the <a href="{home_docs["04162025.pdf"]}" target="_blank">Press Release</a>."""
             ),
             start_date=datetime(2025, 4, 14).date(),
             end_date=None,
