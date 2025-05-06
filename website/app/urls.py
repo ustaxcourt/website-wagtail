c1305141200d359c5e9835961f106dadbf536085
Miest-Moore, Miriam V <mmiest-moore@flexion.us>
2025-05-06 10:46:55 -0700

    removing excess white space

--- website/app/urls.py
+++ website/app/urls.py
@@ -90,7 +90,6 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
