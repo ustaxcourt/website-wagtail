b899ab23d21417e98c67a929e569748c9b65b9dd
Sree Appachi <guruparan18@gmail.com>
2025-05-30 09:59:15 -0400

    enhanced std page as standalone page

--- website/home/models/__init__.py
+++ website/home/models/__init__.py
@@ -1,5 +1,4 @@
 from django.db import models
-from wagtail.contrib.typed_table_block.blocks import TypedTableBlock
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import FieldPanel, InlinePanel, PageChooserPanel
@@ -12,7 +11,6 @@ from datetime import date, datetime
 
 from wagtail.fields import StreamField
 from wagtail import blocks
-from wagtail.images.blocks import ImageBlock
 from wagtail.documents.blocks import DocumentChooserBlock
 from wagtail.snippets.blocks import SnippetChooserBlock
 from wagtail.blocks import PageChooserBlock
@@ -39,26 +37,23 @@ from home.models.judges import (
     JudgeProfile,
     JudgeRole,
 )
-
 from home.models.settings import (
     Footer,  # noqa: F401
     GoogleAnalyticsSettings,  # noqa: F401
 )
-
-from home.models.config import IconCategories
-
+from home.models.config import IconCategories  # noqa: F401
 from home.models.snippets.navigation import (
     NavigationRibbon,  # noqa: F401
     NavigationRibbonLink,  # noqa: F401
 )
-
 from home.models.snippets.common import CommonText  # noqa: F401
-
 from home.models.pages.standard import StandardPage
-
-from home.models.custom_blocks.photo_dedication import PhotoDedicationBlock
-
-from home.models.custom_blocks.common import CommonBlock, link_obj, ColumnBlock
+from home.models.custom_blocks.photo_dedication import PhotoDedicationBlock  # noqa: F401
+from home.models.custom_blocks.common import CommonBlock, link_obj, ColumnBlock  # noqa: F401
+from home.models.custom_blocks.alert_message import AlertMessageBlock  # noqa: F401
+from home.models.custom_blocks.button import ButtonBlock  # noqa: F401
+from home.models.pages.enhanced_standard import EnhancedStandardPage
+from home.models.pages.enhanced_standard import IndentStyle  # noqa: F401
 
 logger = logging.getLogger(__name__)
 
@@ -66,223 +61,6 @@ logger = logging.getLogger(__name__)
 table_value_types = [
     ("text", blocks.RichTextBlock()),
 ]
-
-
-class IndentStyle(models.TextChoices):
-    INDENTED = "indented"
-    UNINDENTED = "unindented"
-
-
-from home.models.custom_blocks.nested_list import create_nested_list_block  # noqa: E402
-
-
-class ButtonBlock(blocks.StructBlock):
-    text = blocks.CharBlock(required=True, help_text="Button text")
-    href = blocks.CharBlock(
-        required=True, help_text="Button link  (Can be relative or absolute)"
-    )
-    style = blocks.ChoiceBlock(
-        choices=[
-            ("primary", "Primary"),
-        ],
-        default="primary",
-        help_text="Choose the button style",
-    )
-
-    class Meta:
-        icon = "placeholder"
-        label = "Button"
-
-
-from home.models.custom_blocks.alert_message import AlertMessageBlock  # noqa: E402
-
-
-class EnhancedStandardPage(Page):
-    class Meta:
-        verbose_name = "Enhanced Standard Page"
-
-    navigation_ribbon = models.ForeignKey(
-        NavigationRibbon,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
-    )
-
-    body = StreamField(
-        [
-            (
-                "heading",
-                blocks.StructBlock(
-                    [
-                        ("text", blocks.CharBlock()),
-                        (
-                            "level",
-                            blocks.ChoiceBlock(
-                                choices=[
-                                    ("h2", "Heading 2"),
-                                    ("h3", "Heading 3"),
-                                    ("h4", "Heading 4"),
-                                    ("h5", "Heading 5"),
-                                ]
-                            ),
-                        ),
-                        (
-                            "id",
-                            blocks.CharBlock(
-                                required=False,
-                                help_text="Optional ID for linking to this heading",
-                            ),
-                        ),
-                    ]
-                ),
-            ),
-            ("h2", blocks.CharBlock(label="Heading 2")),
-            ("h3", blocks.CharBlock(label="Heading 3")),
-            ("h4", blocks.CharBlock(label="Heading 4")),
-            ("paragraph", blocks.RichTextBlock()),
-            ("snippet", SnippetChooserBlock("home.CommonText")),
-            ("button", ButtonBlock()),
-            (
-                "hr",
-                blocks.BooleanBlock(
-                    label="Horizontal Rule",
-                    default=True,
-                    help_text="Add 'Horizontal Rule'.",
-                ),
-            ),
-            (
-                "iframe",
-                blocks.StructBlock(
-                    [
-                        ("src", blocks.URLBlock(required=True)),
-                        ("width", blocks.CharBlock(required=True)),
-                        ("height", blocks.CharBlock(required=True)),
-                        ("class", blocks.CharBlock(required=False)),
-                        ("loading", blocks.CharBlock(required=False)),
-                        ("data_delay", blocks.CharBlock(required=False)),
-                        ("name", blocks.CharBlock(required=False)),
-                        ("title", blocks.CharBlock(required=False)),
-                    ]
-                ),
-            ),
-            (
-                "alert",
-                blocks.StructBlock(
-                    [
-                        (
-                            "alert_type",
-                            blocks.ChoiceBlock(
-                                choices=[
-                                    ("info", "Info"),
-                                    ("success", "Success"),
-                                ],
-                                default="info",
-                            ),
-                        ),
-                        ("content", blocks.RichTextBlock()),
-                    ],
-                ),
-            ),
-            ("image", ImageBlock()),
-            ("photo_dedication", PhotoDedicationBlock()),
-            (
-                "table",
-                TypedTableBlock(
-                    table_value_types,
-                ),
-            ),
-            (
-                "unstyled_table",
-                TypedTableBlock(table_value_types),
-            ),
-            ("list", create_nested_list_block(max_depth=4)),
-            (
-                "links",
-                blocks.StructBlock(
-                    [
-                        (
-                            "class",
-                            blocks.ChoiceBlock(
-                                choices=[
-                                    ("indented", IndentStyle.INDENTED),
-                                    ("unindented", IndentStyle.UNINDENTED),
-                                ],
-                                default=IndentStyle.INDENTED,
-                            ),
-                        ),
-                        ("links", link_obj),
-                    ]
-                ),
-            ),
-            (
-                "questionanswers",
-                blocks.ListBlock(
-                    blocks.StructBlock(
-                        [
-                            ("question", blocks.CharBlock(required=False)),
-                            ("answer", blocks.RichTextBlock()),
-                            ("anchortag", blocks.CharBlock()),
-                        ]
-                    ),
-                    label="Question and Answer",
-                    help_text="Add a question and answer with anchor tag for linking",
-                ),
-            ),
-            ("columns", ColumnBlock()),
-            (
-                "embedded_video",
-                blocks.StructBlock(
-                    [
-                        ("title", blocks.CharBlock(required=False)),
-                        ("description", blocks.RichTextBlock(required=False)),
-                        ("video_url", blocks.URLBlock(required=False)),
-                    ]
-                ),
-            ),
-            (
-                "card",
-                blocks.ListBlock(
-                    blocks.StructBlock(
-                        [
-                            (
-                                "icon",
-                                blocks.ChoiceBlock(
-                                    choices=[
-                                        (
-                                            icon.value,
-                                            icon.name.replace("_", " ").title(),
-                                        )
-                                        for icon in IconCategories
-                                    ],
-                                    required=True,
-                                ),
-                            ),
-                            ("title", blocks.CharBlock(required=True)),
-                            ("description", blocks.RichTextBlock(required=True)),
-                            (
-                                "color",
-                                blocks.ChoiceBlock(
-                                    choices=[
-                                        ("green", "Green"),
-                                        ("yellow", "Yellow"),
-                                    ],
-                                    required=True,
-                                ),
-                            ),
-                        ],
-                        label="Card",
-                    ),
-                    label="Card Set",
-                ),
-            ),
-        ],
-        blank=True,
-    )
-    content_panels = Page.content_panels + [
-        FieldPanel("navigation_ribbon"),
-        FieldPanel("body"),
-    ]
 
 
 judge_snippet = SnippetChooserBlock(
