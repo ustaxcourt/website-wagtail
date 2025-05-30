d1321feb953d7c34b0eebd9f87cc2dc776d700f4
Sree Appachi <guruparan18@gmail.com>
2025-05-30 16:46:56 -0400

    csv upload page

--- website/home/models/__init__.py
+++ website/home/models/__init__.py
@@ -57,6 +57,7 @@ from home.models.pages.case_related_forms import (
 )
 from home.models.pages.directory import DirectoryIndex  # noqa: F401
 from home.models.pages.judge_index import JudgeIndex, JudgeColumnBlock, JudgeColumns  # noqa: F401
+from website.home.models.pages.csv_upload import CSVUploadPage  # noqa: F401
 
 logger = logging.getLogger(__name__)
 
@@ -446,50 +447,3 @@ class JudgesRecruiting(EnhancedStandardPage):
                 context["message"] = message
         context["judges_recruiting"] = filtered_judges
         return context
-
-
-class CSVUploadPage(EnhancedStandardPage):
-    csv_file = models.FileField(upload_to="csv_files/")
-
-    content_panels = EnhancedStandardPage.content_panels + [
-        FieldPanel("csv_file"),
-    ]
-
-    def get_context(self, request, *args, **kwargs):
-        context = super().get_context(request, *args, **kwargs)
-        if self.csv_file:
-            csv_data = self.get_csv_data()
-            if csv_data["headers"] and csv_data["rows"]:
-                context["csv_data"] = csv_data
-            else:
-                context["csv_data"] = None  # Explicitly set to None if no data
-        else:
-            context["csv_data"] = None
-        return context
-
-    def get_csv_data(self):
-        import csv
-        from io import StringIO
-
-        csv_data = {"headers": [], "rows": []}
-
-        # Open the file and read it
-        with self.csv_file.open("r") as file:
-            content = file.read()
-        if isinstance(content, bytes):  # Check if data is in bytes
-            content = content.decode("utf-8")  # Decode bytes to string
-        csv_file = StringIO(content)
-
-        # Parse the CSV
-        csv_reader = csv.reader(csv_file)
-
-        # Get headers from the first row
-        try:
-            csv_data["headers"] = next(csv_reader)
-            csv_data["rows"] = [row for row in csv_reader]
-        except StopIteration:
-            # Handle empty CSV
-            pass
-        finally:
-            pass
-        return csv_data
