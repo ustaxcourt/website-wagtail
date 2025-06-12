bf6b6c7956182c3ddc7f8b6ed5e18e75ab5f079a
Sree Appachi <guruparan18@gmail.com>
2025-06-11 22:30:56 -0400

    revert order

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -66,10 +66,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
