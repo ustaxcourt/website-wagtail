1f7fc716bb9c2866692842a34b1e68545984ea78
Sree Appachi <guruparan18@gmail.com>
2025-06-16 14:23:12 -0400

    format

--- website/home/models/pages/schedule_content.py
+++ website/home/models/pages/schedule_content.py
@@ -1,29 +1,14 @@
-from wagtail.models import Page
-from wagtail.admin.panels import FieldPanel
-
 from wagtail.admin.panels import PublishingPanel
-from wagtail.snippets.blocks import SnippetChooserBlock
-from wagtail.fields import StreamField
-from wagtail import blocks
+
+from home.models.pages.enhanced_standard import EnhancedStandardPage
 
 
-class ScheduledPage(Page):
+class ScheduledPage(EnhancedStandardPage):
     template = "home/enhanced_standard_page.html"
 
     class Meta:
         abstract = False
 
-    body = StreamField(
-        [
-            ("rich_text", blocks.RichTextBlock()),
-            ("snippet", SnippetChooserBlock("home.CommonText")),
-        ],
-        blank=True,
-        use_json_field=True,
-        help_text="Insert text or choose a snippet.",
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel("body"),
+    content_panels = EnhancedStandardPage.content_panels + [
         PublishingPanel(),
     ]
