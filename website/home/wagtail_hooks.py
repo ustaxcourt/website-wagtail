79845f2f66b39d51dacd7ef401dc6cd878e84788
Sree Appachi <guruparan18@gmail.com>
2025-07-02 23:04:57 -0400

    correct syntax

--- website/home/wagtail_hooks.py
+++ website/home/wagtail_hooks.py
@@ -163,7 +163,7 @@ def purge_cloudfront_cache_for_file(file_url):
             cache_path = file_url if file_url.startswith("/") else f"/{file_url}"
 
         if cache_path.startswith("/files/"):
-            cache_path = cache_path.removeprefix("/files/")
+            cache_path = cache_path.removeprefix("/files")
 
         logger.info(f"Purging CloudFront cache for path: {cache_path}")
 
