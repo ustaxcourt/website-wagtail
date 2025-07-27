655603a5852ed673d70ecdc518097168e3ff1b23
Sree Appachi <guruparan18@gmail.com>
2025-07-26 22:47:14 -0400

    Fix environment on migrates

--- website/app/urls.py
+++ website/app/urls.py
@@ -94,6 +94,7 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # Only include debug_toolbar URLs if the app is installed
     if "debug_toolbar" in settings.INSTALLED_APPS:
         urlpatterns += [
