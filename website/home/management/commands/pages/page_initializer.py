d8daff63180ba9930a13bda1e1f0a42391eaca18
Miest-Moore, Miriam V <mmiest-moore@flexion.us>
2025-07-28 12:22:34 -0700

    remove some old logic already abandoned

--- website/home/management/commands/pages/page_initializer.py
+++ website/home/management/commands/pages/page_initializer.py
@@ -20,7 +20,6 @@ logger = logging.getLogger(__name__)
 
 class PageInitializer(ABC):
     DOCUMENTS_BASE_PATH = "home/management/documents"
-    DOCUMENTS_UDPATED_RULES_PATH = "home/management/documents/rules"
     IMAGES_BASE_PATH = "home/management/images"
 
     def __init__(self):
