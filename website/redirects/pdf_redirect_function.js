9c665b03fae018bed8e50a734f46fccd99b060eb
Tejha Bollu <tbollu@flexion.us>
2025-07-29 15:17:17 -0400

    Fix

--- website/redirects/pdf_redirect_function.js
+++ website/redirects/pdf_redirect_function.js
@@ -1,5 +1,9 @@
 function handler(event) {
           var request = event.request;
+          if (request.uri.startsWith('/files/')) {
+            request.uri = request.uri.slice(6);
+          }
+
           var uri = request.uri;
           var pattern = /^\/documents\/Rule-\d+[.\-_A-Za-z0-9]*?(amended|Amended|superseded|2nd|2nd-amended|New|new)[^\/]*\.pdf$/;
           var genericPattern = /^\/documents\/Rule-[\d.]+\.pdf$/;
@@ -15,17 +19,13 @@ function handler(event) {
             };
           }
           if (genericPattern.test(uri)) {
-              return {
-                statusCode: 302,
-                statusDescription: "Found",
-                headers: {
-                  location: { value: uri.toLowerCase() }
-                }
-              };
-            }
-          // Strip /files prefix if present (CloudFront origin routing)
-          if (request.uri.startsWith('/files/')) {
-            request.uri = request.uri.slice(6);
+            return {
+              statusCode: 302,
+              statusDescription: "Found",
+              headers: {
+                location: { value: uri.toLowerCase() }
+              }
+            };
           }
           return request;
         }
