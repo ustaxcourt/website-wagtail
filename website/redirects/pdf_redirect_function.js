b6c00979bcd2597cd565aec130376bd3e5da543d
Tejha Bollu <tbollu@flexion.us>
2025-07-29 18:19:00 -0400

    Testing

--- website/redirects/pdf_redirect_function.js
+++ website/redirects/pdf_redirect_function.js
@@ -5,19 +5,19 @@ function handler(event) {
           }
 
           var uri = request.uri;
-
           // Exact path redirects
           var redirects = {
-            "/files/documents/Complete_Rules_of_Practice_and_Procedure_Amended_080824.pdf": "/files/documents/Complete-Rules-of-Practice-and-Procedure.pdf",
-            "/files/documents/Rule-229A.pdf": "/files/documents/rule-229A.pdf",
-            "/files/documents/Rule-2302nd-amended.pdf": "/files/documents/rule-230.pdf",
-            "/files/documents/Rule-255.1_amended_08082024.pdf": "/files/documents/rule-255.1.pdf",
-            "/files/documents/Rule-255.2New.pdf": "/files/documents/rule-255.2.pdf",
-            "/files/documents/Rule-255.3New.pdf": "/files/documents/rule-255.3.pdf",
-            "/files/documents/Rule-255.4New.pdf": "/files/documents/rule-255.4.pdf",
-            "/files/documents/Rule-255.5New.pdf": "/files/documents/rule-255.5.pdf",
-            "/files/documents/Rule-255.6New.pdf": "/files/documents/rule-255.6.pdf",
-            "/files/documents/Rule-255.7New.pdf": "/files/documents/rule-255.7.pdf"
+            "/documents/Complete_Rules_of_Practice_and_Procedure_Amended_080824.pdf": "/files/documents/Complete-Rules-of-Practice-and-Procedure.pdf",
+            "/documents/Rule-229A.pdf": "/files/documents/rule-229A.pdf",
+            "/documents/Rule-2302nd-amended.pdf": "/files/documents/rule-230.pdf",
+            "/documents/Rule-255.1_amended_08082024.pdf": "/files/documents/rule-255.1.pdf",
+            "/documents/Rule-255.2New.pdf": "/files/documents/rule-255.2.pdf",
+            "/documents/Rule-255.3New.pdf": "/files/documents/rule-255.3.pdf",
+            "/documents/Rule-255.4New.pdf": "/files/documents/rule-255.4.pdf",
+            "/documents/Rule-255.5New.pdf": "/files/documents/rule-255.5.pdf",
+            "/documents/Rule-255.6New.pdf": "/files/documents/rule-255.6.pdf",
+            "/documents/Rule-255.7New.pdf": "/files/documents/rule-255.7.pdf",
+            "/documents/Rule-151_1_Amended_03202023.pdf": "/files/documents/rule-151.1.pdf",
           };
 
           if (redirects[uri]) {
@@ -29,7 +29,7 @@ function handler(event) {
               }
             };
           }
-          // Regex-based matching
+          // Regex-based fallback logic
           var pattern = /^\/documents\/Rule-\d+[.\-_A-Za-z0-9]*?(amended|Amended|superseded|2nd|2nd-amended|New|new)[^\/]*\.pdf$/;
           var genericPattern = /^\/documents\/Rule-[\d.]+\.pdf$/;
 
