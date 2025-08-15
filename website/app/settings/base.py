d7550165546b50f302dda090dab225c3030b06d1
Miest-Moore, Miriam V <mmiest-moore@flexion.us>
2025-08-15 15:42:49 -0700

    Add support for editor email notifications

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.sitemaps",
     "django.contrib.staticfiles",
+    "django_ses",
     "fontawesomefree",
     "social_django",
     "wagtail.contrib.frontend_cache",
@@ -322,7 +323,8 @@ BASE_URL = "http://127.0.0.1:8000"
 # GitHub SHA for build version
 GITHUB_SHA = os.getenv("GITHUB_SHA")
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" TODO: Make environment specific (only prod and testing should send emails)
+EMAIL_BACKEND = 'django_ses.SESBackend'
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -417,3 +419,6 @@ LOGGING = {
 
 SITE_IS_LIVE = date.today() >= date(2025, 6, 1)
 WAGTAILSEARCH_HITS_MAX_AGE = 180  # days
+
+AWS_SES_REGION_NAME = 'us-east-1'
+DEFAULT_FROM_EMAIL = f"noreply@{os.getenv('DOMAIN_NAME')}"
