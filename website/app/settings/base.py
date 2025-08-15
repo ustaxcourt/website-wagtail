28916a9deeba05941db16e02f7614bd355cc9882
Miest-Moore, Miriam V <mmiest-moore@flexion.us>
2025-08-15 16:40:25 -0700

    more keys

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -280,6 +280,8 @@ if aws_bucket_name:
     if os.getenv("AWS_ACCESS_KEY_ID"):
         AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
         AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+        AWS_SES_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+        AWS_SES_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 # Wagtail settings
 WAGTAIL_SITE_NAME = "USTC Website"
