6434711e13e69dee7af93427eab9e7eeec671d49
Joel <joel.quainoo.ctr@ustaxcourt.gov>
2025-08-18 10:10:44 -0400

    WAG-420: fixing editing on dawson page

--- website/home/templatetags/dawson_extras.py
+++ website/home/templatetags/dawson_extras.py
@@ -4,15 +4,17 @@ import re
 register = template.Library()
 
 DEADLINE_RE = re.compile(r"(?<!\w)(DEADLINE FOR FILING:)")
-DAWSON_BREAK_RE = re.compile(r"\.\s+(DAWSON has been designed\b)")
+TIME_PHRASE_RE = re.compile(r"11:59\s*p\.m\.,\s*Eastern time,", re.IGNORECASE)
+DAWSON_BREAK_RE = re.compile(r"(\bdue\.)\s+(DAWSON has been designed\b)")
 
 
 @register.filter
 def highlight_labels(value: str) -> str:
     if not isinstance(value, str):
         return value
+    value = TIME_PHRASE_RE.sub(r"<strong>\g<0></strong>", value, count=1)
     value = DEADLINE_RE.sub(
         r'<strong class="callout-label">\1</strong>', value, count=1
     )
-    value = DAWSON_BREAK_RE.sub(r".<br>&nbsp;\1", value, count=1)
+    value = DAWSON_BREAK_RE.sub(r"\1<br>&nbsp;\2", value, count=1)
     return value
