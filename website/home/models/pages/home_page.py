408a98983e4b173f5582249d6a5e2af317fa7cbb
Sree Appachi <guruparan18@gmail.com>
2025-08-24 22:55:59 -0400

    cleanup get_context

--- website/home/models/pages/home_page.py
+++ website/home/models/pages/home_page.py
@@ -28,8 +28,6 @@ class HomePage(Page):
     def get_context(self, request):
         context = super().get_context(request)
         context["now"] = timezone.now()
-        live_entries = []
-        context["static_text_cards"] = live_entries
         return context
 
 
