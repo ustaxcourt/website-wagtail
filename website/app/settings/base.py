fb190f4acef5dd7e418aa801bf8dfdf77efcac87
Miest-Moore, Miriam V <mmiest-moore@flexion.us>
2025-08-26 10:01:48 -0700

    both?

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -320,7 +320,7 @@ GOOGLE_ANALYTICS_ID = "G-3T6ZS0FHZ8"
 
 ENVIRONMENT = "dev"
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = f"https://{os.getenv("DOMAIN_NAME")}"
 
 # GitHub SHA for build version
 GITHUB_SHA = os.getenv("GITHUB_SHA")
