e984872c06ae2a2153dd0dea280a72bc3a48ded5
yilake-mehari <Yilake.Mehari.ctr@ustaxcourt.gov>
2025-09-02 11:37:32 -0400

    Adding migration file and editing backfill_live_revision management command

---
+++ website/home/migrations/0068_backfill_live_revisions.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+from django.core.management import call_command
+
+
+def run_backfill_live_revisions(apps, schema_editor):
+    "Run custom backfill_live_revisions management command"
+
+    try:
+        call_command("backfill_live_revisions", "--dry-run=False")
+    except Exception as e:
+        print(f"Warning: backfill_live_revisions failed: {e}")
+
+
+def reverse_backfill_live_revisions(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [("home", "0067_alter_enhancedstandardpage_body")]
+    operations = [
+        migrations.RunPython(
+            run_backfill_live_revisions,
+            reverse_backfill_live_revisions,
+        ),
+    ]
