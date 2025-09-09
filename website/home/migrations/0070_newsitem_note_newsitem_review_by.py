0a61d18024751b7b561e890d4065a39a71e460c5
Jim deVos <jim.devos@gmail.com>
2025-09-09 10:48:54 -0700

    renamed migration file to resolve conflict

--- website/home/migrations/0070_newsitem_note_newsitem_review_by.py
+++ website/home/migrations/0070_newsitem_note_newsitem_review_by.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("home", "0069_homepage_static_text_cards"),
+        ("home", "0070_backfill_live_revisions"),
     ]
 
     operations = [
