508c0e11430841243a2cdfc27ea83c213ae6a114
Jim deVos <jim.devos@gmail.com>
2025-09-23 13:56:10 -0700

    move now() out of loop, remove from context

--- website/home/models/pages/home_page.py
+++ website/home/models/pages/home_page.py
@@ -53,13 +53,12 @@ class HomePage(ModerationMixin, Page):
 
     def get_context(self, request):
         context = super().get_context(request)
-        context["now"] = timezone.now()
 
         # Filter static text cards based on start/end dates
         live_static_text_cards = []
+        now = timezone.now()
         for card_block in self.static_text_cards:
             card = card_block.value
-            now = timezone.now()
 
             # Check if card should be displayed
             start_valid = not card.get("start_date") or card.get("start_date") <= now
