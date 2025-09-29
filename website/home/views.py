4117f705c2bfc7e68f97a7fbb9ee4b5052fac420
Ahmed Mohamed <ahmedomar3215@gmail.com>
2025-09-29 16:22:49 -0400

    Fix url from displaying duplicate

--- website/home/views.py
+++ website/home/views.py
@@ -91,7 +91,7 @@ class NewsItemReportView(ReportView):
         "publish_date",
         "homepage_display_expiration_date",
         "banner_options",
-        "document.urldocument_url",
+        "document_url",
     ]
 
     def __init__(self, **kwargs):
