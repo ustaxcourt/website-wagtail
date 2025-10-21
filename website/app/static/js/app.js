c5d3581086ce1fb789a455aee8f6824b53adb4f0
Ahmed Mohamed <ahmedomar3215@gmail.com>
2025-10-21 17:08:08 -0400

    Button sits on top of footer, but shakes violently when they overlap scrolling down slowly

--- website/app/static/js/app.js
+++ website/app/static/js/app.js
@@ -4,8 +4,25 @@
  */
 function refreshScrollToTopButton() {
     const button = document.getElementById('scroll-to-top');
+    const footer = document.getElementById('app-footer');
+
+    const buttonRect = button.getBoundingClientRect();
+    const footerRect = footer.getBoundingClientRect();
+
+    const viewportHeight = window.innerHeight;
+    const distanceFromBottom = viewportHeight - footerRect.top;
+
+
     if (window.scrollY > 100) {
         button.classList.add('visible');
+
+        if (buttonRect.bottom > footerRect.top) {
+            //console.log(footerRect.top);
+
+            button.style.bottom = `${20 + distanceFromBottom}px`;
+        } else {
+            button.style.bottom = '20px';
+        }
     } else {
         button.classList.remove('visible');
     }
