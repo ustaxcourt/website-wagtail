cade9b912740c446aa31876793f8c136c40b7834
jeanobame11 <jeanobame99@gmail.com>
2025-12-04 11:09:48 -0600

    added qa.py file

---
+++ website/app/settings/qa.py
@@ -0,0 +1,27 @@
+from .base import *  # noqa: F403
+from .base import MIDDLEWARE
+import os
+from datetime import date
+
+
+try:
+    from .local import *  # noqa: F403
+
+except ImportError:
+    pass
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = False
+BASE_URL = f"https://{os.getenv('DOMAIN_NAME')}"
+ENVIRONMENT = "qa"
+
+MIDDLEWARE = ["app.middleware.JSONExceptionMiddleware"] + MIDDLEWARE
+
+SITE_IS_LIVE = date.today() >= date(2999, 6, 1)
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAILADMIN_BASE_URL = f"https://{os.getenv('DOMAIN_NAME')}"
+
+WAGTAILADMIN_NOTIFICATION_USE_HTML = True
