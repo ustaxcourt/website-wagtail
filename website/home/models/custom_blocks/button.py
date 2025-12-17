6acf65a9efcdc4efb23e4b804e37e5040e44af40
Ahmed Mohamed <ahmedomar3215@gmail.com>
2025-12-17 17:08:14 -0500

    Style button and add fields

--- website/home/models/custom_blocks/button.py
+++ website/home/models/custom_blocks/button.py
@@ -1,10 +1,16 @@
 from wagtail import blocks
+from home.blocks import SVGChooserBlock
 
 
 class ButtonBlock(blocks.StructBlock):
+    # make icon able to hold images in the future
+    icon = SVGChooserBlock(required=False)
     text = blocks.CharBlock(required=True, help_text="Button text")
     href = blocks.CharBlock(
         required=True, help_text="Button link  (Can be relative or absolute)"
+    )
+    url = blocks.URLBlock(
+        required=True, help_text="Optional: Use this field for absolute URLs"
     )
     style = blocks.ChoiceBlock(
         choices=[
