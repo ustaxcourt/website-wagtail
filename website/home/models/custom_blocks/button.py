1456704c499119b679415358e7ad610b1aed0ff4
Ahmed Mohamed <ahmedomar3215@gmail.com>
2025-12-18 16:37:15 -0500

    Fix link handling

--- website/home/models/custom_blocks/button.py
+++ website/home/models/custom_blocks/button.py
@@ -7,8 +7,7 @@ class ButtonBlock(blocks.StructBlock):
     # make icon able to hold images in the future
     icon = SVGDocumentChooserBlock(required=False, help_text="Optional: Button icon")
     text = blocks.CharBlock(required=True, help_text="Button text")
-    url = blocks.CharBlock(required=True, help_text="Button link", label="URL")
-    link = blocks.StreamBlock(
+    url = blocks.StreamBlock(
         [
             ("internal_page", PageChooserBlock(help_text="Select a page to link to")),
             ("external_url", blocks.URLBlock(help_text="Enter an external URL")),
@@ -16,6 +15,7 @@ class ButtonBlock(blocks.StructBlock):
         max_num=1,
         min_num=1,
         help_text="Choose either an internal page or external URL",
+        label="URL",
     )
 
     style = blocks.ChoiceBlock(
