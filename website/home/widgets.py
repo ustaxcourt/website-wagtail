ce64b1521f91b3b36c138bfff9b87d3936ffd7e2
Ahmed Mohamed <ahmedomar3215@gmail.com>
2025-12-22 11:43:47 -0500

    Implement feedback from demo

--- website/home/widgets.py
+++ website/home/widgets.py
@@ -2,6 +2,8 @@ from wagtail.documents.widgets import AdminDocumentChooser
 
 
 class AdminSVGChooser(AdminDocumentChooser):
+    choose_one_text = "Choose an SVG"
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.chooser_modal_url_name = "svg_chooser:choose"
