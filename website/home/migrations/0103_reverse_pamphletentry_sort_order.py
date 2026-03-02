57480ef46298a429afebc5b7c14654000323045e
Jim deVos <jim.devos@gmail.com>
2026-03-02 13:32:58 -0700

    pulling in latest main; sorting migrations

--- website/home/migrations/0103_reverse_pamphletentry_sort_order.py
+++ website/home/migrations/0103_reverse_pamphletentry_sort_order.py
@@ -24,7 +24,7 @@ def reverse_pamphlet_sort_order(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("home", "0101_alter_pamphletentry_options_and_more"),
+        ("home", "0102_alter_pamphletentry_options_and_more"),
     ]
 
     operations = [
