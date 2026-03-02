649aa1f121e1d9eed2bea7fc148c5b5d23277bc5
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-03-02 12:27:38 -0500

    Created Cypress test to verify that the list of components that can be added to the body of an Enhanced Standard Page match a list of expected components.

---
+++ website/cypress/e2e/admin_edit_enhancedstandardpage_validation.cy.ts
@@ -0,0 +1,64 @@
+/**
+ * Enhanced Standard Page Page Edit Validation
+ *
+ * This test logs into the Wagtail admin, adds a new Enhanced Standard Page
+ * to the Home directory, and verifies the components that can be added
+ * to the page's body.
+ */
+
+import { text } from "stream/consumers";
+
+describe('Enhanced Standard Page Edit Validation', () => {
+  const ADMIN_USERNAME = 'admin';
+  const ADMIN_PASSWORD = 'ustcAdminPW!';
+
+  let allPages: any[] = [];
+  let testResults: Array<{
+    pageId: string;
+    title: string;
+    slug?: string;
+    status: 'success' | 'failed';
+    error?: string;
+  }> = [];
+
+  it('logs in and verifies expected components can be added to a new Enhanced Standard Page', () => {
+    // Login to admin first
+    cy.visit('/admin/login/');
+    cy.get('input[name="username"]').type(ADMIN_USERNAME);
+    cy.get('input[name="password"]').type(ADMIN_PASSWORD);
+    cy.get('button[type="submit"], input[type="submit"]').click();
+    cy.url({ timeout: 10000 }).should('include', '/admin/');
+
+    //Navigate to the "New: Enhanced Standard Page" page in Wagtail Admin for adding a page in the Home folder
+    cy.visit('/admin/pages/add/home/enhancedstandardpage/3/');
+    cy.url({ timeout: 10000 }).should('include', '/admin/pages/add/home/enhancedstandardpage/3');
+
+    //Click the "+" button under Body
+    cy.get('button.c-sf-add-button').click();
+
+    let componentsExpectedToFind: string[] = ["Heading", "Heading 2", "Heading 3", "Heading 4", "Paragraph", "Indented Paragraph", "Snippet", "Button",
+        "Horizontal Rule", "Iframe", "Alert", "Image", "Photo Dedication", "Table", "Unstyled table", "Enhanced Table", "List", "List of Links",
+        "Question and Answer", "Columns", "Embedded video", "Card Set", "Accordion Block", "Callout Block", "Card Tiles", "Quick Access Tiles"
+    ];
+    //Find the element that represents the menu that appears when the "+" button is clicked
+    cy.get('div#downshift-0-menu').within(() => {
+        //Get each item in that menu. These represent a component that could be added to the Enhanced Standard Page.
+        cy.get('div.w-combobox__option-text').each(($el, index, $list) => {
+
+            //Determine if component is in the array of expected components
+            let textToSearchFor: string = $el.text();
+            let message: string = 'Component "' + textToSearchFor + '" is in list of expected components';
+            expect(componentsExpectedToFind, message).to.include(textToSearchFor);
+
+            //Update the array to remove the found component
+            if (componentsExpectedToFind.includes(textToSearchFor))
+            {
+                componentsExpectedToFind = componentsExpectedToFind.filter(x => x != textToSearchFor);
+            }
+        }).then(() => {
+            //Determine if there are any components that were not found in the menu that should be displayed.
+            expect(componentsExpectedToFind).to.be.empty;
+        });
+    });
+  });
+});
