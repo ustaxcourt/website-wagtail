fde6920f71a6520f93dcfb716f5120046f9fd2cd
Ahmed Mohamed <ahmedomar3215@gmail.com>
2026-03-04 15:46:37 -0500

    Add trusted sites option in settings

--- website/home/models/__init__.py
+++ website/home/models/__init__.py
@@ -10,6 +10,7 @@ from home.models.settings import (
     Footer,
     GoogleAnalyticsSettings,
     GoogleTagManagerSettings,
+    TrustedSites,
 )
 from home.models.config import IconCategories
 from home.models.snippets.news_item import NewsItem
