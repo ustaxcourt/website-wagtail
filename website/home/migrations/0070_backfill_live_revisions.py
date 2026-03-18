54db431088c87ce8dc6b71acf26e79c7ad40f768
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-03-18 13:54:05 -0400

    Reverting change made to 0070_backfill_live_revisions.py

--- website/home/migrations/0070_backfill_live_revisions.py
+++ website/home/migrations/0070_backfill_live_revisions.py
@@ -1,15 +1,14 @@
 from django.db import migrations
-# from django.core.management import call_command
+from django.core.management import call_command
 
 
 def run_backfill_live_revisions(apps, schema_editor):
     "Run custom backfill_live_revisions management command"
 
-    # try:
-    #     call_command("backfill_live_revisions")
-    # except Exception as e:
-    #     print(f"Warning: backfill_live_revisions failed: {e}")
-    pass
+    try:
+        call_command("backfill_live_revisions")
+    except Exception as e:
+        print(f"Warning: backfill_live_revisions failed: {e}")
 
 
 def reverse_backfill_live_revisions(apps, schema_editor):
