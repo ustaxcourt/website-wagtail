e1d938ce5d4dff590a06359044e700918e7f0823
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-03-27 09:00:07 -0400

    Added WAGTAILTRANSFER_LOOKUP_FIELDS to settings to try to fix error when transferring an image in a collection.

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -456,23 +456,11 @@ WAGTAILTRANSFER_SOURCES = get_json_from_os_env("WAGTAILTRANSFER_SOURCES")
 
 WAGTAILTRANSFER_SOURCES_JSON = os.getenv("WAGTAILTRANSFER_SOURCES", "{}")
 
-
-# WAGTAILTRANSFER_SECRET_KEY = "temporary_secret_key_for_testing"
-
-# WAGTAILTRANSFER_SOURCES = {
-#     'Stephen': {
-#         'BASE_URL': 'https://stephen-sandbox-web.ustaxcourt.gov/wagtail-transfer/',
-#         'SECRET_KEY': 'SuperSecretWagtailTransferKey$123',
-#     },
-#     'Ahmed': {
-#         'BASE_URL': 'https://ahmed-sandbox-web.ustaxcourt.gov/wagtail-transfer/',
-#         'SECRET_KEY': 'WAGTRANSFER!1098%',
-#     },
-# }
-
 WAGTAILTRANSFER_NO_FOLLOW_MODELS = [
     "wagtailcore.page",
     "contenttypes.contenttype",
     "auth.user",
     "wagtailcore.revision",
 ]
+
+WAGTAILTRANSFER_LOOKUP_FIELDS = {"wagtailcore.collection": ["name"]}
