53972a709062427c550effbb63c2444a21b0974a
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-04-02 15:36:29 -0400

    Removed reference to wagtail_transfer.admin_urls as they are not needed by Wagtail Transfer.

--- website/app/urls.py
+++ website/app/urls.py
@@ -17,7 +17,6 @@ from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.documents.models import Document
 from wagtail_transfer import urls as wagtailtransfer_urls
-from wagtail_transfer import admin_urls as wagtailtransfer_admin_urls
 
 logger = logging.getLogger(__name__)
 
@@ -109,7 +108,6 @@ urlpatterns = [
         search_views.definitions_search,
         name="definitions_search",
     ),
-    path("admin/wagtail-transfer/", include(wagtailtransfer_admin_urls)),
     path("wagtail-transfer/", include(wagtailtransfer_urls)),
 ]
 
