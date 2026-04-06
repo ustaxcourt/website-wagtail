5ff38209af46f6f8ba76a4d88390d99846fefbba
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-04-06 11:59:00 -0400

    Attempt to resolve error when transferring a page derived from home.standardpage.

--- website/app/settings/base.py
+++ website/app/settings/base.py
@@ -461,6 +461,7 @@ WAGTAILTRANSFER_NO_FOLLOW_MODELS = [
     "contenttypes.contenttype",
     "auth.user",
     "wagtailcore.revision",
+    "home.standardpage",
 ]
 
 WAGTAILTRANSFER_LOOKUP_FIELDS = {
