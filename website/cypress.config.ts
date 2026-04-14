e1405c358b68f57b855e5e509ad5cc38f40ed318
segwurube-ustc <samuel.egwurube.ctr@ustaxcourt.gov>
2026-04-14 10:25:57 -0500

    first take

--- website/cypress.config.ts
+++ website/cypress.config.ts
@@ -1,6 +1,9 @@
+/// <reference types="node" />
+
 import { defineConfig } from "cypress";
 
 const includeAdminValidation = process.env.CYPRESS_INCLUDE_ADMIN_VALIDATION === 'true';
+const adminValidationSpecPattern = '**/admin*_validation.cy.{js,jsx,ts,tsx}';
 
 export default defineConfig({
   e2e: {
@@ -18,10 +21,10 @@ export default defineConfig({
         },
       })
     },
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     excludeSpecPattern: includeAdminValidation
       ? []
-      : ['**/admin_page_validation.cy.{js,jsx,ts,tsx}']
+      : [adminValidationSpecPattern]
   },
 });
