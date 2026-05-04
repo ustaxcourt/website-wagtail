2865fb4d6fada92df1244295c1a9845e98e54929
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-05-04 15:11:25 -0400

    Created migration script to create redirect links based on an Excel file. Updated wagtail_hooks.py to update the location of the redirects for the local environment so they work with the paths used by that environment.

--- website/home/wagtail_hooks.py
+++ website/home/wagtail_hooks.py
@@ -14,6 +14,7 @@ from wagtail import hooks
 from wagtail.admin.mail import send_mail
 from wagtail.admin.menu import MenuItem
 from wagtail.contrib.frontend_cache.utils import purge_pages_from_cache, PurgeBatch
+from wagtail.contrib.redirects.models import Redirect
 from wagtail.documents.models import Document
 from wagtail.images.models import Image
 from wagtail.models import Page
@@ -224,6 +225,29 @@ def purge_cloudfront_cache_for_file(file_url):
         batch.purge()
 
         logger.info(f"Successfully purged CloudFront cache for path: {cache_path}")
+
+        # The local environment's Document URL path is /documents/{doc_id}/{file_name}.
+        # Other environments' Document URL path is /files/documents/{file_name}.
+        # Redirects that are created as part of home migration 0115 use the Document URL path for non-local environments.
+        # The code in this if block updates the redirects to use the local environment Document URL path if run against a local environment.
+        # This code will also update redirects for images or any other items that are transmitted via CloudFront in non-local environments.
+        if settings.ENVIRONMENT == "local":
+            logger.debug(f"Checking for redirect to update for {cache_path}")
+            last_slash_index = cache_path.rfind("/")
+            if last_slash_index != -1:
+                file_name_to_find_in_redirect = cache_path[last_slash_index + 1 :]
+                if Redirect.objects.filter(
+                    redirect_link__iendswith=file_name_to_find_in_redirect
+                ).exists():
+                    redirect_to_update = Redirect.objects.get(
+                        redirect_link__iendswith=file_name_to_find_in_redirect
+                    )
+                    logger.debug(
+                        f"Updating redirect with redirect_link: {redirect_to_update.redirect_link} to {cache_path}"
+                    )
+                    redirect_to_update.redirect_link = cache_path
+                    redirect_to_update.save()
+                    logger.debug("Redirect updated")
 
     except Exception as e:
         logger.error(
