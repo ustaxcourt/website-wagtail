ac2b01edbcf7ae5985419b58d786dba334d4c4e5
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-05-20 16:33:12 -0400

    Fixed issues caused by custom link extractor handlers not recursively checking custom fields in custom table components. Moved custom link extractor handlers into custom_link_extractor as conditionals within its extract_from_value() method.

--- website/home/utils/custom_link_extractor.py
+++ website/home/utils/custom_link_extractor.py
@@ -2,19 +2,52 @@ from wagtail_external_links_report.utils import LinkExtractor
 from bs4 import BeautifulSoup
 from wagtail.rich_text import RichText
 from wagtail.contrib.typed_table_block.blocks import TypedTable
+from home.models.custom_blocks.button import ButtonBlock
+from home.blocks import QuickAccessTileBlock
+from home.models.pages.enhanced_standard import CardTileBlock
+from home.models.custom_blocks.image_with_link import ImageWithLinkBlock
+from wagtail.blocks import StructValue
 
 
 class CustomLinkExtractor(LinkExtractor):
+    def _get_external_link_with_text_from_attribute(value, text_attribute):
+        links = []
+        if value["url"]:
+            for child in value["url"]:
+                if hasattr(child, "block_type") and child.block_type == "external_url":
+                    links.extend({"text": value[text_attribute], "url": child.value})
+
+        return links
+
     def extract_from_value(self, value):
         """Recursively extract links depending on value type."""
         links = []
 
+        if isinstance(value, StructValue) and isinstance(value, ButtonBlock):
+            links.extend(self._get_external_link_with_text_from_attribute("text"))
+            return links
+
+        if isinstance(value, StructValue) and isinstance(value, QuickAccessTileBlock):
+            links.extend(self._get_external_link_with_text_from_attribute("title"))
+            return links
+
+        if isinstance(value, StructValue) and isinstance(value, CardTileBlock):
+            links.extend(
+                self._get_external_link_with_text_from_attribute("card_header")
+            )
+            return links
+
+        if isinstance(value, StructValue) and isinstance(value, ImageWithLinkBlock):
+            links.extend(self._get_external_link_with_text_from_attribute("image"))
+            return links
+
         # Handles external links in Enhanced Tables, Styled Tables, and Unstyled Tables
-        # Other custom handlers are called by similar logic in the base LinkExtractor's
-        # extract_from_value() method.
-        for block_cls, handler in self.custom_handlers.items():
-            if isinstance(value, TypedTable) and isinstance(value, block_cls):
-                return handler(value)
+        if isinstance(value, TypedTable):
+            for row in value.rows:
+                for cell in row:
+                    links.extend(self.extract_from_value(cell.value))
+
+            return links
 
         if isinstance(value, RichText):
             html = value.source
