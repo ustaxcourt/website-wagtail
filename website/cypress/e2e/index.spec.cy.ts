bb005239044a8a30b2eb0f20782630557a3016f4
Jim deVos <james.devos@ustaxcourt.gov>
2026-05-20 11:26:51 -0700

    WAG-122: moved judges test to its own file

--- website/cypress/e2e/index.spec.cy.ts
+++ website/cypress/e2e/index.spec.cy.ts
@@ -64,24 +64,4 @@ describe('index page', () => {
       expect($button.eq(2).attr('title')).to.contain('Search') //test mobile search box
     });
   })
-
-  it('should hit the users endpoint and display expected elements', () => {
-    cy.visit('/judges');
-
-    cy.get('.usa-button').should('have.length.at.least', 1);
-    cy.get('.usa-button').first().then(($link) => {
-      const expectedUrl = $link.attr('href');
-
-      cy.wrap($link).click();
-
-      cy.url().should('include', expectedUrl);
-
-      cy.get('.back-to-judges').should('be.visible');
-      cy.get('.judge-name').should('be.visible').and('not.be.empty');
-      cy.get('.judge-bio').should('be.visible').and('not.be.empty');
-    });
-
-    checkA11y();
-
-  })
 })
