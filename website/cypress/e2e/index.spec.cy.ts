d4e915bdee2beb7cc643515bd38375586a36f152
segwurube-ustc <samuel.egwurube.ctr@ustaxcourt.gov>
2026-06-17 09:26:37 -0500

    earliest updates

--- website/cypress/e2e/index.spec.cy.ts
+++ website/cypress/e2e/index.spec.cy.ts
@@ -95,7 +95,7 @@ describe('index page', () => {
     cy.get('[data-testid="search-button"]').should(($button) => {
 
       expect($button).to.have.length(3) //checking that home page has three search buttons
-      expect($button.eq(0)).to.contain('Search') //test tablet search box
+      expect($button.eq(0).attr('aria-label')).to.contain('Search') //test tablet search box (icon-only, uses aria-label)
       expect($button.eq(1).attr('aria-label')).to.contain('Search') //test desktop search box (icon-only, uses aria-label)
       expect($button.eq(2).attr('title')).to.contain('Search') //test mobile search box
     });
