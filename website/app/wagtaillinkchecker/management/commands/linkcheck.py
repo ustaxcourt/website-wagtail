67d01865b354948baea5e8bf0bfe9cae7fccb8b9
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-06-22 11:30:59 -0400

    Removed logic for sending an email to a page's author when a broken link is found in a page via the linkcheck management command.

--- website/app/wagtaillinkchecker/management/commands/linkcheck.py
+++ website/app/wagtaillinkchecker/management/commands/linkcheck.py
@@ -1,8 +1,4 @@
-from django.conf import settings
-from django.core import mail
-from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand
-from django.template.loader import render_to_string
 
 from app.wagtaillinkchecker.scanner import broken_link_scan
 from app.wagtaillinkchecker.models import ScanLink
@@ -12,11 +8,7 @@ from wagtail.models import Site
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument(
-            "--do-not-send-mail",
-            action="store_true",
-            help="Do not send mails when finding broken links",
-        )
+        pass
 
     def handle(self, *args, **kwargs):
         site = Site.objects.filter(is_default_site=True).first()
@@ -30,46 +22,3 @@ class Command(BaseCommand):
         print(
             f"Found {len(total_links)} total links, with {len(broken_links)} broken links."
         )
-
-        if kwargs.get("do_not_send_mail"):
-            print("Will not send any emails")
-            return
-
-        messages = []
-        for page in pages:
-            revisions = page.revisions
-            user = None
-            user_email = settings.DEFAULT_FROM_EMAIL
-            if revisions:
-                revision = revisions.latest("created_at")
-                user = revision.user
-                user_email = revision.user.email if revision.user else ""
-            page_broken_links = []
-            for link in broken_links:
-                if link.page == page:
-                    page_broken_links.append(link)
-            if not page_broken_links:
-                continue
-            email_message = render_to_string(
-                "wagtaillinkchecker/emails/broken_links.html",
-                {
-                    "page_broken_links": page_broken_links,
-                    "user": user,
-                    "page": page,
-                    "base_url": site.root_url,
-                    "site_name": settings.WAGTAIL_SITE_NAME,
-                },
-            )
-            email = EmailMessage(
-                'Broken links on page "%s"' % (page.title),
-                email_message,
-                settings.DEFAULT_FROM_EMAIL,
-                [user_email],
-            )
-            email.content_subtype = "html"
-            messages.append(email)
-
-        connection = mail.get_connection()
-        connection.open()
-        connection.send_messages(messages)
-        connection.close()
