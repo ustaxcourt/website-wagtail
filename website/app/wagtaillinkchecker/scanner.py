5e3f2e7c7e1ac3f60662fd5ad101e33998d0343f
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-06-22 21:22:25 -0400

    Added logging to debug sandbox URL issues

--- website/app/wagtaillinkchecker/scanner.py
+++ website/app/wagtaillinkchecker/scanner.py
@@ -118,9 +118,12 @@ def broken_link_scan(site, verbosity=1, sync=False):
     scan = Scan.objects.create(site=site)
 
     domain_name = getattr(settings, "BASE_URL", site.root_url)
+    print(f"broken_link_scan - site.root_url: {site.root_url}")
+    print(f"broken_link_scan - domain_name: {domain_name}")
 
     try:
         for page in pages:
+            print(f"broken_link_scan - page.full_url: {page.full_url}")
             url = page.full_url.replace(site.root_url, domain_name)
             if verbosity > 1:
                 print(f"Checking {url}")
