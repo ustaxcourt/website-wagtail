d2cf7605be1e6798348daf5e7b1f2b28126c2cef
segwurube-ustc <samuel.egwurube.ctr@ustaxcourt.gov>
2026-06-27 04:04:15 -0500

    infra issues fix

--- infra/modules/app/rds.tf
+++ infra/modules/app/rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "default" {
   identifier_prefix = "${var.environment}-"
   allocated_storage   = 10
   engine              = "postgres"
-  engine_version      = "16.8"
+  engine_version      = "16"
   instance_class      = "db.t3.micro"
   username            = "master"
   password            = var.database_password
