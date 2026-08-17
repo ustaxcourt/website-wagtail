a7d17a934f10b05e94356d2a7c330ca448df15c5
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-08-17 13:26:21 -0400

    Initial checkin

---
+++ website/home/models/pages/petitioner_experience.py
@@ -0,0 +1,6 @@
+from home.models.pages.enhanced_standard import EnhancedStandardPage
+
+
+class PetititonerExperiencePage(EnhancedStandardPage):
+    class Meta:
+        verbose_name = "Petitioner Experience Page"
