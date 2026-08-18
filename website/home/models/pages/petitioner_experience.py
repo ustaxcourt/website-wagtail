6ababe5ab1d1c4eeef51bef3124073f969d6526d
Stephen Cook <stephen.cook.ctr@ustaxcourt.gov>
2026-08-18 10:07:22 -0400

    Updates to meet AC of story

--- website/home/models/pages/petitioner_experience.py
+++ website/home/models/pages/petitioner_experience.py
@@ -1,6 +1,41 @@
+from django.db import models
+from wagtail.models import Page
 from home.models.pages.enhanced_standard import EnhancedStandardPage
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
+from home.models.custom_blocks.common import custom_promote_panels
+from home.admin.moderation import ModerationTabbedInterface
+from home.forms import ReviewByRequiredOnSubmitForm
 
 
-class PetititonerExperiencePage(EnhancedStandardPage):
+class PetitionerExperienceReviewByRequiredOnSubmitForm(ReviewByRequiredOnSubmitForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["navigation_ribbon"].required = True  # Force required in admin
+
+
+class PetitionerExperiencePage(EnhancedStandardPage):
+    base_form_class = PetitionerExperienceReviewByRequiredOnSubmitForm
+
     class Meta:
         verbose_name = "Petitioner Experience Page"
+
+    introductory_text = models.TextField(
+        help_text="Text to be displayed at the top of the page under the page's title.",
+        blank=False,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("show_floating_definitions_button"),
+        FieldPanel("navigation_ribbon"),
+        FieldPanel("introductory_text"),
+        FieldPanel("body"),
+    ]
+
+    edit_handler = ModerationTabbedInterface.create_for_page(
+        content_panels=content_panels, promote_panels=custom_promote_panels
+    )
+
+    search_fields = EnhancedStandardPage.search_fields + [
+        index.SearchField("introductory_text"),
+    ]
