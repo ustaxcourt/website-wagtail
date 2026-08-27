a3050d1d53aac577a0bdeda9a9461de586754be8
Ahmed Mohamed <ahmedomar3215@gmail.com>
2026-08-27 12:47:17 -0400

    Fix call to action box being recreated after pages referencing it were created

--- website/home/management/commands/create_pages.py
+++ website/home/management/commands/create_pages.py
@@ -22,7 +22,10 @@ from home.management.commands.pages.redirect_page import RedirectPageInitializer
 from home.management.commands.pages.footer import FooterInitializer
 from home.management.commands.pages.navigation import NavigationInitializer
 
-from home.management.commands.snippets import snippets_to_initialize
+from home.management.commands.snippets import (
+    snippets_to_initialize,
+    snippets_to_initialize_via_executescript,
+)
 from home.management.commands.redirects.redirect_initializer import RedirectInitializer
 
 from home.management.commands.pages.documents import UnlistedFiles
@@ -32,6 +35,9 @@ from home.management.commands.pages.efiling_and_case_maintenance.dawson_page imp
 )
 from home.management.commands.pages.card_tiles_test_page import (
     CardTilesTestPageInitializer,
+)
+from home.management.commands.snippets.call_to_action_box import (
+    CallToActionBoxInitializer,
 )
 
 # Initialize home and footer first
@@ -43,7 +49,8 @@ home_page_initialize = [
 
 # Ensure Home Page is initialized first
 pages_to_initialize = home_page_initialize + (
-    about_the_court_pages_to_initialize
+    [CallToActionBoxInitializer]
+    + about_the_court_pages_to_initialize
     + rules_and_guidance_pages_to_initialize
     + orders_and_opinions_pages_to_initialize
     + efiling_and_case_maintenance_pages_to_initialize
@@ -76,6 +83,10 @@ class Command(BaseCommand):
             for snippet_class in snippets_to_initialize:
                 snippet_instance = snippet_class()
                 snippet_instance.create()
+
+        for snippet_class in snippets_to_initialize_via_executescript:
+            snippet_instance = snippet_class()
+            snippet_instance.run()
 
         for page_class in pages_to_initialize:
             page_instance = page_class()
