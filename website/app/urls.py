910722037f496086149b89435d58a3053237f0ea
segwurube-ustc <samuel.egwurube.ctr@ustaxcourt.gov>
2026-09-15 11:17:12 -0500

    for sandbox

--- website/app/urls.py
+++ website/app/urls.py
@@ -135,7 +135,7 @@ if settings.DEBUG:
         ),
     )
 
-    # Serve static and media files from development server
+    # Serve static and media files from development server.
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
