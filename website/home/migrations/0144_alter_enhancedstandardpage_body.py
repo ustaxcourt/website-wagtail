f54d44548fb623488fc1272bd32a322f5c4c5a8a
segwurube-ustc <samuel.egwurube.ctr@ustaxcourt.gov>
2026-09-22 17:13:55 -0500

    merged migrations

--- website/home/migrations/0144_alter_enhancedstandardpage_body.py
+++ website/home/migrations/0144_alter_enhancedstandardpage_body.py
@@ -6,7 +6,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("home", "0143_merge_20260916_1200"),
+        ("home", "0143_alter_enhancedstandardpage_body_pe"),
     ]
 
     operations = [
